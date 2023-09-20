321d5784b1252daa16c1cc1ccffc1d0b7cc36fc6
KKK <heipo_nogu@outlook.jp>
2023-09-20 17:48:59 +0900

    add models

---
+++ models/transformer.cpp
@@ -0,0 +1,19 @@
+#include "activations.h"
+#include "arrays.h"
+#include "datasets.h"
+#include "derivatives.h"
+#include "initializers.h"
+#include "linalg.h"
+#include "losses.h"
+#include "mathematics.h"
+#include "metrics.h"
+#include "preprocessing.h"
+#include "random.h"
+#include "regularizations.h"
+
+#include <array>
+#include <random>
+
+int main() {
+    
+}
