6d89714fd6818d48f8d246ffd4f8ca7f1c157797
kimi <heipo_nogu@outlook.jp>
2023-09-26 10:56:59 +0900

    now I created the sequences

--- models/rnn.cpp
+++ models/rnn.cpp
@@ -26,20 +26,16 @@ int main() {
     TrainTest train_test = train_test_split(air_passengers, 0.33, 42); // TODO: Don't forget to shuffle in train_test_split!
 
     // Create sequences and corresponding labels for training
-    short sequence_length = 12; // Use the previous 12 months to predict the next month
-    Tensor X_train = Tensor({ 0.0f }, { train_data.x_first._size, sequence_length, 1 });
-    Tensor y_train = Tensor({ 0.0f }, { train_data.x_first._size, 1 });
+    unsigned int sequence_length = 12; // Use the previous 12 months to predict the next month
+    Tensor X_train = Tensor({ 0.0f }, { train_test.x_first._size - sequence_length, sequence_length, 1 });
+    Tensor y_train = Tensor({ 0.0f }, { train_test.x_first._size - sequence_length, 1 });
 
-    // TODO: use int for all for loop in this project
     int idx = 0;
-    for (int i = 0; i < 84 * 12; ++i) {
+    for (int i = 0; i < (train_test.x_first._size - sequence_length) * sequence_length; ++i) {
+        if (i % sequence_length == 0 && i != 0)
+            idx -= sequence_length - 1;
         X_train[i] = train_test.x_first[idx];
-        if (i % sequence_length == 0 && i != 0) {
-            idx = (i == sequence_length) ? i - 11 : i - (11 * 2);
-            std::cout << "i: " << i << std::endl;
-            std::cout << "idx: " << idx << std::endl;
-            continue;
-        }
+        // X_train[i] = i <= sequence_length ? train_test.x_first[idx] : train_test.x_first[idx - sequence_length - 1];
         ++idx;
     }
 
@@ -47,13 +43,6 @@ int main() {
         y_train[i] = train_test.x_first[i + sequence_length];
     }
 
-    // for i in range(len(train_data) - sequence_length):
-    //     X_train.append(train_data[i:i + sequence_length])
-    //     y_train.append(train_data[i + sequence_length])
-
-    // X_train, y_train = np.array(X_train), np.array(y_train)
-
     std::cout << X_train << std::endl;
     std::cout << y_train << std::endl;
-    std::cout << y_train._num_ch_dim << std::endl;
 }
