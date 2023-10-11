e218d3e47ac88f0957758f87dd8a1535330069ea
KKK <heipo_nogu@outlook.jp>
2023-10-12 06:15:55 +0900

    add todo

--- src/main.cpp
+++ src/main.cpp
@@ -336,6 +336,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     Tensor q_table = zeros({ n_observations, n_actions });
     std::cout << q_table << std::endl;
 
+    // number of episode we will run
+    int n_episodes = 10000;
+
+    // maximum of iteration per episode
+    int max_iter_episode = 100;
+
+    // initialize the exploration probability to 1
+    int exploration_proba = 1;
+
+    // exploartion decreasing decay for exponential decreasing
+    float exploration_decreasing_decay = 0.001f;
+
+    // minimum of exploration proba
+    float min_exploration_proba = 0.01f;
+
+    // discounted factor
+    float gamma = 0.99f;
+
+    float lr = 0.1f;
+
     // Making the window
     const char CLASS_NAME[] = "Sample Window Class";
 
