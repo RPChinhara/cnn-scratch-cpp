0041eaaa02fad292b443d13e153f537d5953aae0
KKK <heipo_nogu@outlook.jp>
2023-10-14 13:01:34 +0900

    remove unneccessary header files

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
-#include "activations.h"
-#include "arrays.h"
 #include "datasets.h"
-#include "derivatives.h"
-#include "initializers.h"
-#include "linalg.h"
-#include "losses.h"
-#include "mathematics.h"
-#include "metrics.h"
 #include "preprocessing.h"
-#include "random.h"
-#include "regularizations.h"
 #include "nn.h"
 
-#include <array>
-#include <random>
 #include <windows.h>
 // #pragma comment (lib, "User32.lib")
 
