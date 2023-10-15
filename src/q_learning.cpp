c0b7c5629f516f84f82680b7c967b853787b0058
KKK <heipo_nogu@outlook.jp>
2023-10-16 07:30:05 +0900

    adding new functions

--- src/q_learning.cpp
+++ src/q_learning.cpp
@@ -11,3 +11,31 @@ QLearning::QLearning(unsigned int n_states, unsigned int n_actions, float learni
     this->exploration_min = exploration_min;
     this->q_table = zeros({ n_states, n_actions });
 }
+
+void QLearning::choose_action(unsigned int state) {
+//  # Exploit
+//         if np.random.rand() > self.exploration_rate:
+//             return np.argmax(self.q_table[state, :])
+//         # Explore
+//         return np.random.randint(0, self.n_actions)
+
+    // Exploit
+    if np.random.rand() > exploration_rate:
+    // Explore
+
+    // create
+    np.random.rand()
+    np.random.randint(
+}
+
+void QLearning::update(unsigned int state, unsigned int action, float reward, float next_state) {
+    # Q-learning update rule
+        best_next_action = np.argmax(self.q_table[next_state, :])
+        q_target = reward + self.discount_factor * self.q_table[next_state, best_next_action]
+        q_delta = q_target - self.q_table[state, action]
+        self.q_table[state, action] += self.learning_rate * q_delta
+        
+        # Exploration rate decay
+        if self.exploration_rate > self.exploration_min:
+            self.exploration_rate *= self.exploration_decay
+}
