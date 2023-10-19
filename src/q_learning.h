2aa588a1102d1bf44605c2e55894621ade654795
kimi <heipo_nogu@outlook.jp>
2023-10-19 22:47:49 +0900

    new todo and house keeping

--- src/q_learning.h
+++ src/q_learning.h
@@ -4,7 +4,7 @@
 
 class QLearning {
 public:
-    QLearning(unsigned int n_states, unsigned int n_actions, float learning_rate = 0.01f, float discount_factor = 0.95, float exploration_rate = 1.0f, float exploration_decay = 0.995f, float exploration_min = 0.01f);
+    QLearning(unsigned int n_states, unsigned int n_actions, float learning_rate = 0.01f, float discount_factor = 0.95f, float exploration_rate = 1.0f, float exploration_decay = 0.995f, float exploration_min = 0.01f);
     unsigned int choose_action(unsigned int state);
     void update(unsigned int state, unsigned int action, float reward, unsigned int next_state);
 
