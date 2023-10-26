2ec83eb352b70b569cfed1ccab19f09ef0f63e0e
mon <yukawa_hidenori@icloud.com>
2023-10-26 22:39:18 +0900

    removing all the unnessesary implemention from in the env

--- src/environment.cpp
+++ src/environment.cpp
@@ -4,7 +4,6 @@
 #include <windows.h>
 
 Environment::Environment() {
-    // new
     days_lived                   = 0;
     thirsty_days                 = 0;
     max_days                     = 50;
@@ -13,12 +12,11 @@ Environment::Environment() {
 }
 
 void Environment::render() {
-    std::cout << "Current guess: " << insert_space(current_guess) << std::endl;
-    std::cout << "Attempts made: " << this->attempts_made << std::endl;
+    std::cout << "Current guess: " << std::endl;
+    std::cout << "Attempts made: " << std::endl;
 }
 
 std::unordered_map<std::string, std::string> Environment::reset() {
-    // new
     days_lived                   = 0;
     thirsty_days                 = 0;
     current_state["thirstiness"] = "neutral";
@@ -27,41 +25,16 @@ std::unordered_map<std::string, std::string> Environment::reset() {
 }
 
 std::tuple<std::string, int, bool> Environment::step(const std::pair<int, char>& action) {
-    if (done) {
+    if (true) {
         MessageBox(NULL, "The game is over. Please reset the environment.", "Error", MB_ICONERROR | MB_OK);
         ExitProcess(1);
     }
 
-    // Unpack the action, which is expected to be a tuple of (position, letter)
-    int position = action.first;
-    char letter  = action.second;
+    bool done = false;
 
-    if (secret_word[position] == letter)
-        current_guess[position] = letter;
-
-    attempts_made += 1;
-
-    if (attempts_made >= max_attempts) {
-        done = true;
-        return std::make_tuple(insert_space(current_guess), -1, done); // -1 reward for failure
-    }
-    
-    if (current_guess == secret_word) {
-        done = true;
-        return std::make_tuple(insert_space(current_guess), 1, done); // 1 reward for failure
-    }
-
-    return std::make_tuple(insert_space(current_guess), 0, done); // 0 reward for failure
+    return std::make_tuple("cat", 0, done); // 0 reward for failure
 }
 
-std::string Environment::insert_space(std::string& str) {
-    std::string spaced_string;
-    for (size_t i = 0; i < str.length(); ++i) {
-        spaced_string += str[i];
-        // Add a space after each character, except for the last character
-        if (i < str.length() - 1) {
-            spaced_string += ' ';
-        }
-    }
-    return spaced_string;
+void Environment::update_thirstiness() {
+
 }
--- src/environment.h
+++ src/environment.h
@@ -11,15 +11,8 @@ public:
     std::unordered_map<std::string, std::string> reset();
     std::tuple<std::string, int, bool> step(const std::pair<int, char>& action);
 private:
-    std::string insert_space(std::string& str);
-    
-    std::string secret_word;
-    std::string current_guess;
-    int max_attempts;
-    unsigned int attempts_made;
-    bool done;
+    void update_thirstiness();
 
-    // new
     unsigned short days_lived;
     unsigned short thirsty_days;
     unsigned short max_days; // The desired number of days to live
