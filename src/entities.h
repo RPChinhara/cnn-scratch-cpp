30325c1c55bc9b08fbb49ea4cdaa7d36d8d1bec3
mon <yukawa_hidenori@icloud.com>
2023-12-03 13:45:00 +0900

    house keeping

--- src/entities.h
+++ src/entities.h
@@ -1,8 +1,6 @@
 #pragma once
 
-// The dimensions of the client area (the inner content area) of the window is 1904 x 1041, on the other hand the size of the entire window (including borders and title bar) is 1920 x 1080 as it set when creating the Windows application. Retrieve by those codes below.
-
-RECT agent  = { 13, 981, 63, 1031 }; // Left, Top, Right, Bottom coordinates
+RECT agent  = { 13, 981, 63, 1031 };
 RECT agent2 = { 1849, 986, 1899, 1036 };
 RECT food   = { 5, 5, 55, 55 };
 RECT water  = { 1849, 5, 1899, 55 };
