3e151be4d46aa03fd6c70c418edcd9627eedf039
mon <yukawa_hidenori@icloud.com>
2023-12-05 01:12:51 +0900

    house keeping

--- src/environment.cpp
+++ src/environment.cpp
@@ -17,49 +17,42 @@ int Environment::reset() {
 }
 
 std::tuple<int, int, bool> Environment::step(const std::string& action) {
-    // Use std::find to search for the action in the actions
     auto it = std::find(actions.begin(), actions.end(), action);
 
-    // If action is not in actions
     if (it == actions.end()) {
         MessageBox(nullptr, "Invalid action.", "Error", MB_ICONERROR);
         ExitProcess(1);
     }
     
-    // Implement how the state changes based on the agent's actions
     if (action == "eat" && current_state != std::distance(states.begin(), std::find(states.begin(), states.end(), "full")))
         current_state = std::min(current_state + 1, num_states - 1);
     else if (action != "eat" && current_state != std::distance(states.begin(), std::find(states.begin(), states.end(), "hungry")))
         current_state = std::max(current_state - 1, 0);
 
-    // Increment days lived by 1
     days_lived += 1;
 
-    // Check if the agent has eaten
     if (action == "eat")
         days_without_eating = 0;
     else
         days_without_eating += 1;
 
-    // Calculate the reward based on the environment's state
     int reward = calculate_reward();
-    bool done  = check_termination();
+    bool done = check_termination();
 
     return std::make_tuple(current_state, reward, done);
 }
 
 int Environment::calculate_reward() {
-    // Define rewards and penalties based on the environment's state
-    if (current_state == std::distance(states.begin(), std::find(states.begin(), states.end(), "hungry")) || days_without_eating >= max_days_without_eating)
-        return -1; // Penalize for being hungry
-    else if (days_lived >= max_days) {
+    if (current_state == std::distance(states.begin(), std::find(states.begin(), states.end(), "hungry")) || days_without_eating >= max_days_without_eating) {
+        return -1;
+    } else if (days_lived >= max_days) {
         days_lived = 0;
-        return 1; // Reward for living the desired number of days
-    } else
-        return 0; // No additional reward or penalty
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 bool Environment::check_termination() {
-    // Check if the termination conditions are met
     return days_without_eating >= max_days_without_eating;
 }
