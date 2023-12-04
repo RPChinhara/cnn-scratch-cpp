7ca2467534144bd4ebf726145d77e730bbe92c64
mon <yukawa_hidenori@icloud.com>
2023-12-04 13:33:05 +0900

    house keeping

--- src/preprocessing.cpp
+++ src/preprocessing.cpp
@@ -15,17 +15,6 @@ Tensor one_hot(const Tensor& in, const unsigned short depth) {
 
     std::vector<float> indices;
 
-    // If 'in' was { 0, 1, 2 }, and 'depth' was 3, then this function should return
-    // 1 0 0
-    // 0 1 0
-    // 0 0 1
-    //
-    // 0 1 2
-    // 3 4 5
-    // 6 7 8
-    //
-    // If 'in' was { 0, 0, 0 }, then indices should become { 0, 3, 6 }.
-    // If 'in' was { 0, 1, 2 }, then indices should become { 0, 4, 8 }.
     for (unsigned int i = 0; i < in._size; ++i) {
         if (i == 0)
             indices.push_back(in[i]);
@@ -48,12 +37,13 @@ TrainTest train_test_split(const Tensor& x, const float test_size, const unsigne
     
     TrainTest feature;
     feature.x_first  = Tensor({ 0.0 }, { (unsigned int)(std::floorf(x._shape.front() * (1.0 - test_size))), x._shape.back() });
-    feature.x_second = Tensor({ 0.0 }, { (unsigned int)(std::ceilf(x._shape.front() * test_size)),          x._shape.back() });
+    feature.x_second = Tensor({ 0.0 }, { (unsigned int)(std::ceilf(x._shape.front() * test_size)), x._shape.back() });
 
     for (unsigned int i = 0; i < feature.x_first._size; ++i)
         feature.x_first[i] = new_x[i];
 
     unsigned int idx = 0;
+
     for (unsigned int i = feature.x_first._size; i < x._size; ++i) {
         feature.x_second[idx] = new_x[i];
         ++idx;
@@ -76,6 +66,7 @@ TrainTest2 train_test_split(const Tensor& x, const Tensor& y, const float test_s
         train_test.x_first[i] = new_x[i];
 
     unsigned int idx = 0;
+
     for (unsigned int i = train_test.x_first._size; i < x._size; ++i) {
         train_test.x_second[idx] = new_x[i];
         ++idx;
@@ -85,6 +76,7 @@ TrainTest2 train_test_split(const Tensor& x, const Tensor& y, const float test_s
         train_test.y_first[i] = new_y[i];
 
     idx = 0;
+
     for (unsigned int i = train_test.y_first._size; i < y._size; ++i) {
         train_test.y_second[idx] = new_y[i];
         ++idx;
