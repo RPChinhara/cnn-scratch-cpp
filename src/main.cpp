b6da1745c79e80023ae25a132fcfb9eaff6919a7
mon <yukawa_hidenori@icloud.com>
2023-12-04 14:30:47 +0900

    -add FreeConsole() -delete one of TrainTest()

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,32 @@
 #include "window.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Create a console for logging otherwise I can't when WinMain() is used as the entry point because it doesn't use the standard console for input and output
     AllocConsole();
     freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
 
 #if 1
-    // Feedforward neural network
-    // Load the Iris dataset
     Iris iris = load_iris();
     Tensor x = iris.features;
     Tensor y = iris.target;
 
-    // Preprocess the dataset
     y = one_hot(y, 3);
-    TrainTest2 train_temp = train_test_split(x, y, 0.2, 42);
-    TrainTest2 val_test = train_test_split(train_temp.x_second, train_temp.y_second, 0.5, 42);
+    TrainTest train_temp = train_test_split(x, y, 0.2, 42);
+    TrainTest val_test = train_test_split(train_temp.x_second, train_temp.y_second, 0.5, 42);
     train_temp.x_first = min_max_scaler(train_temp.x_first);
     val_test.x_first = min_max_scaler(val_test.x_first);
     val_test.x_second = min_max_scaler(val_test.x_second);
 
-    // Train and test the neural network
     NN nn = NN({ 4, 128, 3 }, 0.01f);
     nn.train(train_temp.x_first, train_temp.y_first, val_test.x_first, val_test.y_first);
     nn.predict(val_test.x_second, val_test.y_second);
 #endif
 
-    // Initialize the Windows application
     try {
         Window window(hInstance, nCmdShow);
-        return window.messageLoop();
+        int result = window.messageLoop();
+        FreeConsole();
+        return result;
     } catch (const std::exception& e) {
-        // Handle the error, e.g., show an error message
         MessageBox(nullptr, e.what(), "Error", MB_ICONERROR | MB_OK);
         return -1;
     }
--- src/preprocessing.cpp
+++ src/preprocessing.cpp
@@ -32,31 +32,11 @@ Tensor one_hot(const Tensor& in, const unsigned short depth) {
     return out;
 }
 
-TrainTest train_test_split(const Tensor& x, const float test_size, const unsigned int random_state) {
-    Tensor new_x = x;
-    
-    TrainTest feature;
-    feature.x_first  = Tensor({ 0.0 }, { (unsigned int)(std::floorf(x._shape.front() * (1.0 - test_size))), x._shape.back() });
-    feature.x_second = Tensor({ 0.0 }, { (unsigned int)(std::ceilf(x._shape.front() * test_size)), x._shape.back() });
-
-    for (unsigned int i = 0; i < feature.x_first._size; ++i)
-        feature.x_first[i] = new_x[i];
-
-    unsigned int idx = 0;
-
-    for (unsigned int i = feature.x_first._size; i < x._size; ++i) {
-        feature.x_second[idx] = new_x[i];
-        ++idx;
-    }
-
-    return feature;
-}
-
-TrainTest2 train_test_split(const Tensor& x, const Tensor& y, const float test_size, const unsigned int random_state) {
+TrainTest train_test_split(const Tensor& x, const Tensor& y, const float test_size, const unsigned int random_state) {
     Tensor new_x = shuffle(x, random_state);
     Tensor new_y = shuffle(y, random_state);
 
-    TrainTest2 train_test;
+    TrainTest train_test;
     train_test.x_first  = Tensor({ 0.0 }, { (unsigned int)(std::floorf(x._shape.front() * (1.0 - test_size))), x._shape.back() });
     train_test.x_second = Tensor({ 0.0 }, { (unsigned int)(std::ceilf(x._shape.front() * test_size)),          x._shape.back() });
     train_test.y_first  = Tensor({ 0.0 }, { (unsigned int)(std::floorf(y._shape.front() * (1.0 - test_size))), y._shape.back() });
