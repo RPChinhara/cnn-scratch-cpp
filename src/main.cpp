6fa5420316bd25116b417650572a4c6ed9180317
KKK <heipo_nogu@outlook.jp>
2023-12-20 05:36:46 +0900

    house keeping

--- src/main.cpp
+++ src/main.cpp
@@ -13,40 +13,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     AllocConsole();
     freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
 
-    // std::cout << 0 % 3 << std::endl;
-    // std::cout << 1 % 3 << std::endl;
-    // std::cout << 2 % 3 << std::endl;
-    // std::cout << 3 % 3 << std::endl;
-    // std::cout << 4 % 3 << std::endl;
-    // std::cout << 5 % 3 << std::endl;
-
-    Tensor a1 = Ones({ 2, 3, 4, 2 });
-    Tensor a = Ones({ 2, 3 });
-    Tensor af = Zeros({ 2, 3 });
-    Tensor b = Ones({ 3, 2 });
-    Tensor c = Ones({ 2, 2, 3 });
-    Tensor ffdf = NormalDistribution( { 2, 3 });
-    Tensor df = UniformDistribution( { 2, 3, 3 });
-
-    // std::cout << a + b << std::endl;
-    // std::cout << a + c << std::endl;
-    // std::cout << d + e << std::endl;
-    // std::cout << d + f << std::endl;
-    // std::cout << Transpose(d) << std::endl;
-    // std::cout << d.num_ch_dim << std::endl;
-    // std::cout << h.num_ch_dim << std::endl;
-    // std::cout << ffdf << std::endl;
-    // std::cout << df << std::endl;
-    // std::cout << MatMul(a, b) << std::endl;
-    std::cout << ffdf << std::endl;
-    std::cout << df << std::endl;
-
-    for (int i = 0; i < af.size; ++i) {
-        af[i] += 7.0f;
-    }
-
-    std::cout << af << std::endl;
-
     try {
         Window window(hInstance, nCmdShow);
         int result = window.MessageLoop();
