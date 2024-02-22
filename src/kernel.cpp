f0c98a724b04cf0e9a03708bac87d8c8f0adcbbf
mon <yukawa_hidenori@icloud.com>
2024-02-23 01:39:22 +0900

    house keeping

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,48 +1,49 @@
 #include "kernel.h"
 
-__global__ void Exp(float *in, float *out, size_t n)
+__global__ void Exp(float *tensor, float *newTensor, size_t n)
 {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (id < n)
-        out[id] = expf(in[id]);
+        newTensor[id] = expf(tensor[id]);
 }
 
-__global__ void Log(float *in, float *out, size_t n)
+__global__ void Log(float *tensor, float *newTensor, size_t n)
 {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (id < n)
-        out[id] = logf(in[id]);
+        newTensor[id] = logf(tensor[id]);
 }
 
-__global__ void MatMul(float *in1, float *in2, float *out, size_t m, size_t n, size_t k)
+__global__ void MatMul(float *tensor1, float *tensor2, float *newTensor, size_t numRowsTensor1, size_t numColsTensor1,
+                       size_t numRowsTensor2)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (i < m && j < k)
+    if (i < numRowsTensor1 && j < numRowsTensor2)
     {
         float sum = 0.0;
 
-        for (size_t l = 0; l < n; l++)
-            sum += in1[i * n + l] * in2[l * k + j];
+        for (size_t l = 0; l < numColsTensor1; l++)
+            sum += tensor1[i * numColsTensor1 + l] * tensor2[l * numRowsTensor2 + j];
 
-        out[i * k + j] = sum;
+        newTensor[i * numRowsTensor2 + j] = sum;
     }
 }
 
-// __global__ void OperatorPlus(float *in1, float *in2, float *out, size_t otherShapeBack, size_t n)
+// __global__ void OperatorPlus(float *in1, float *in2, float *newTensor, size_t otherShapeBack, size_t n)
 // {
 //     int id = blockIdx.x * blockDim.x + threadIdx.x;
 //     if (id < n)
-// 	    out[id] = in1[id] + in2[id];
+// 	    newTensor[id] = in1[id] + in2[id];
 // }
 
-__global__ void Relu(float *in, float *out, size_t n)
+__global__ void Relu(float *tensor, float *newTensor, size_t n)
 {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (id < n)
-        out[id] = fmaxf(0.0f, in[id]);
+        newTensor[id] = fmaxf(0.0f, tensor[id]);
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,7 +1,7 @@
 #pragma once
 
-__global__ void Exp(float *in, float *out, size_t n);
-__global__ void Log(float *in, float *out, size_t n);
-__global__ void MatMul(float *in1, float *in2, float *out, size_t m, size_t n, size_t k);
+__global__ void Exp(float *tensor, float *newTensor, size_t n);
+__global__ void Log(float *tensor, float *newTensor, size_t n);
+__global__ void MatMul(float *tensor1, float *tensor2, float *newTensor, size_t numRowsTensor1, size_t numColsTensor1, size_t numRowsTensor2);
 // __global__ void OperatorPlus(float *in1, float *in2, float *out, size_t otherShapeBack, size_t k);
-__global__ void Relu(float *in, float *out, size_t n);
+__global__ void Relu(float *tensor, float *newTensor, size_t n);
