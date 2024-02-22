3e607120262f89f41d62d30c171da83b95bf8166
mon <yukawa_hidenori@icloud.com>
2024-02-23 01:53:24 +0900

    house keeping

--- src/physics.h
+++ src/physics.h
@@ -4,5 +4,5 @@
 #include "entity.h"
 
 void ResolveBoundaryCollision(Agent &agent, const LONG client_width, const LONG client_height);
-void ResolveRectanglesCollision(Agent &agent, const RECT &rect2, Entity entity, const LONG client_width,
+void ResolveRectanglesCollision(Agent &agent, const RECT &entity, Entity entityType, const LONG client_width,
                                 const LONG client_height);
