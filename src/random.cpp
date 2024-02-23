65edd9ea1ab1108461792655abea501ea1ae551d
KKK <heipo_nogu@outlook.jp>
2024-02-23 22:41:15 +0900

    house keeping

--- src/random.cpp
+++ src/random.cpp
@@ -6,49 +6,49 @@
 
 Tensor NormalDistribution(const std::vector<size_t> &shape, const float mean, const float stddev)
 {
-    Tensor out = Tensor();
+    Tensor newTensor = Tensor();
 
     for (const size_t &i : shape)
         assert(i != 0);
 
-    out.shape = shape;
+    newTensor.shape = shape;
 
-    if (out.shape.size() > 0)
+    if (newTensor.shape.size() > 0)
     {
         size_t num_elem = 1;
 
         for (const size_t &i : shape)
             num_elem *= i;
 
-        out.size = num_elem;
+        newTensor.size = num_elem;
     }
     else
     {
-        out.size = 1;
+        newTensor.size = 1;
     }
 
-    out.elem = new float[out.size];
+    newTensor.elem = new float[newTensor.size];
 
     std::random_device rd;
     std::mt19937 rng(rd());
     std::normal_distribution<float> dist(mean, stddev);
 
-    for (size_t i = 0; i < out.size; ++i)
-        out[i] = dist(rng);
+    for (size_t i = 0; i < newTensor.size; ++i)
+        newTensor[i] = dist(rng);
 
-    if (out.shape.size() > 0)
+    if (newTensor.shape.size() > 0)
     {
-        out.num_ch_dim = 1;
+        newTensor.num_ch_dim = 1;
 
         for (const size_t &i : shape)
-            out.num_ch_dim *= i;
+            newTensor.num_ch_dim *= i;
     }
     else
     {
-        out.num_ch_dim = 0;
+        newTensor.num_ch_dim = 0;
     }
 
-    return out;
+    return newTensor;
 }
 
 Tensor Shuffle(const Tensor &tensor, const size_t random_state)
