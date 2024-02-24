82266a5182ff0aff0617d016e0462e26028b865f
KKK <heipo_nogu@outlook.jp>
2024-02-25 00:50:20 +0900

    house keeping

--- src/array.h
+++ src/array.h
@@ -4,7 +4,7 @@
 
 class Tensor;
 
-Tensor ClipByValue(const Tensor &tensor, float clip_val_min, float clip_val_max);
+Tensor ClipByValue(const Tensor &tensor, float clipValMin, float clipValMax);
 Tensor Slice(const Tensor &tensor, const size_t begin, const size_t size);
 Tensor Transpose(const Tensor &tensor);
 Tensor Zeros(const std::vector<size_t> &shape);
