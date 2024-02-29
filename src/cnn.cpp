8c636e9b94ba35297747fe2dfb13194634dc58f6
kimi <heipo_nogu@outlook.jp>
2024-03-01 04:43:01 +0900

    init kernel

--- src/cnn.cpp
+++ src/cnn.cpp
@@ -1,5 +1,6 @@
 #include "cnn.h"
 
+#include "array.h"
 #include "tensor.h"
 
 CNN2D::CNN2D(const std::vector<size_t> &filters, float const learning_rate)
@@ -10,6 +11,7 @@ CNN2D::CNN2D(const std::vector<size_t> &filters, float const learning_rate)
 
 void CNN2D::Train(const Tensor &xTrain, const Tensor &yTrain, const Tensor &xVal, const Tensor &yVal)
 {
+    Tensor kernel = Zeros({3, 3});
 }
 
 void CNN2D::Predict(const Tensor &xTest, const Tensor &yTest)
