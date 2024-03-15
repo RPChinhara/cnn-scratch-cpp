e442a0ed3f30e6e301aa886c0810ef0d4e8d2302
kimi <heipo_nogu@outlook.jp>
2024-03-16 00:03:52 +0900

    variables comes first

--- src/entities/agent.h
+++ src/entities/agent.h
@@ -21,6 +21,30 @@ enum Orientation
 class Agent
 {
   public:
+    Direction direction = Direction::SOUTH;
+    Orientation orientation = Orientation::FRONT;
+    RECT position;
+    RECT previousPosition;
+    RECT leftEyePosition;
+    RECT rightEyePosition;
+
+    const LONG width = 50;
+    const LONG height = 50;
+
+    const LONG eye_width = 5;
+    const LONG eye_height = 13;
+
+    const LONG toEyeWidth = 13;
+    const LONG toEyeHeight = 10;
+
+    bool has_collided_with_food = false;
+    bool has_collided_with_mod = false;
+    bool has_collided_with_wall = false;
+    bool has_collided_with_water = false;
+
+    bool render_agent_left_eye = true;
+    bool render_agent_right_eye = true;
+
     Agent() = default;
     Agent(LONG clientWidth, LONG clientHeight)
     {
@@ -60,28 +84,4 @@ class Agent
         }
         return *this;
     }
-
-    Direction direction = Direction::SOUTH;
-    Orientation orientation = Orientation::FRONT;
-    RECT position;
-    RECT previousPosition;
-    RECT leftEyePosition;
-    RECT rightEyePosition;
-
-    const LONG width = 50;
-    const LONG height = 50;
-
-    const LONG eye_width = 5;
-    const LONG eye_height = 13;
-
-    const LONG toEyeWidth = 13;
-    const LONG toEyeHeight = 10;
-
-    bool has_collided_with_food = false;
-    bool has_collided_with_mod = false;
-    bool has_collided_with_wall = false;
-    bool has_collided_with_water = false;
-
-    bool render_agent_left_eye = true;
-    bool render_agent_right_eye = true;
 };
