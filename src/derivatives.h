69b52784b0dd6e8f9409688dbc97df3ed1d9c30c
kimi <heipo_nogu@outlook.jp>
2024-04-02 00:55:40 +0900

    delete dmse_dsigmoid_dz()

--- src/derivatives.h
+++ src/derivatives.h
@@ -5,5 +5,4 @@
 class Tensor;
 
 Tensor dl_da_da_dz(const Tensor &y_target, const Tensor &y_pred, Act act);
-Tensor dmse_dsigmoid_dsigmoid_dz(const Tensor &y_target, const Tensor &y_pred);
 Tensor da_dz(const Tensor &tensor, Act act);
