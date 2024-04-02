89edf07af347e1d6432f763918cf956012c9f334
kimi <heipo_nogu@outlook.jp>
2024-04-03 00:35:40 +0900

    change the order

--- src/diffs.cpp
+++ src/diffs.cpp
@@ -1,24 +1,6 @@
 #include "diffs.h"
 #include "ten.h"
 
-Ten dl_da_da_dz(const Ten &y_target, const Ten &y_pred, Act act)
-{
-    switch (act)
-    {
-    case SIGMOID: {
-        return (y_pred - y_target) * y_pred * (1 - y_pred);
-        break;
-    }
-    case SOFTMAX: {
-        return (y_pred - y_target);
-        break;
-    }
-    default:
-        std::cout << "Unknown activation." << std::endl;
-        return Ten();
-    }
-}
-
 Ten da_dz(const Ten &ten, Act act)
 {
     Ten newTensor = ten;
@@ -37,6 +19,24 @@ Ten da_dz(const Ten &ten, Act act)
         }
 
         return newTensor;
+        break;
+    }
+    default:
+        std::cout << "Unknown activation." << std::endl;
+        return Ten();
+    }
+}
+
+Ten dl_da_da_dz(const Ten &y_target, const Ten &y_pred, Act act)
+{
+    switch (act)
+    {
+    case SIGMOID: {
+        return (y_pred - y_target) * y_pred * (1 - y_pred);
+        break;
+    }
+    case SOFTMAX: {
+        return (y_pred - y_target);
         break;
     }
     default:
--- src/diffs.h
+++ src/diffs.h
@@ -4,5 +4,5 @@
 
 class Ten;
 
-Ten dl_da_da_dz(const Ten &y_target, const Ten &y_pred, Act act);
 Ten da_dz(const Ten &ten, Act act);
+Ten dl_da_da_dz(const Ten &y_target, const Ten &y_pred, Act act);
