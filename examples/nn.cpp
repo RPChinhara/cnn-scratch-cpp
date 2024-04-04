3f48a89064d80a1a480b27ef127db614739f84f5
kimi <heipo_nogu@outlook.jp>
2024-04-05 07:06:03 +0900

    done with refactoring nn files back to transformer model

--- examples/nn.cpp
+++ examples/nn.cpp
@@ -6,31 +6,32 @@
 
 int main()
 {
-    Iris iris = LoadIris();
-    Ten features = iris.features;
-    Ten targets = iris.targets;
+    iris data = load_iris();
+    Ten features = data.features;
+    Ten targets = data.targets;
 
-    targets = OneHot(targets, 3);
+    targets = one_hot(targets, 3);
 
-    TrainTest train_temp = TrainTestSplit(features, targets, 0.2, 42);
-    TrainTest val_test = TrainTestSplit(train_temp.testFeatures, train_temp.testTargets, 0.5, 42);
+    TrainTest train_temp = train_test_split(features, targets, 0.2, 42);
+    TrainTest val_test = train_test_split(train_temp.test_features, train_temp.test_targets, 0.5, 42);
 
-    train_temp.trainFeatures = MinMaxScaler(train_temp.trainFeatures);
-    val_test.trainFeatures = MinMaxScaler(val_test.trainFeatures);
-    val_test.testFeatures = MinMaxScaler(val_test.testFeatures);
+    train_temp.train_features = min_max_scaler(train_temp.train_features);
+    val_test.train_features = min_max_scaler(val_test.train_features);
+    val_test.test_features = min_max_scaler(val_test.test_features);
 
-    nn nn = nn({4, 128, 3}, 0.01f);
+    nn classifier = nn({4, 128, 3}, 0.01f);
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    nn.Train(train_temp.trainFeatures, train_temp.trainTargets, val_test.trainFeatures, val_test.trainTargets);
+    classifier.train(train_temp.train_features, train_temp.train_targets, val_test.train_features,
+                     val_test.train_targets);
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
     std::cout << "Time taken: " << duration.count() << " seconds\n";
 
-    nn.Predict(val_test.testFeatures, val_test.testTargets);
+    classifier.pred(val_test.test_features, val_test.test_targets);
 
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,17 @@
-// #include "datasets\englishspanish.h"
-// #include "models\transformer.h"
+#include "datasets\enes.h"
+#include "models\trans.h"
 
-// #include <iostream>
-// #include <windows.h>
-
-// int main()
-// {
-//     SetConsoleOutputCP(CP_UTF8);
-
-//     EnEs en_es = load_en_es();
-
-//     for (int i = 0; i < en_es.targetRaw.size(); ++i)
-//         std::cout << en_es.targetRaw[i] << " " << en_es.contextRaw[i] << std::endl;
-
-//     Transformer transformer = Transformer();
-
-//     return 0;
-// }
-
-#include "datasets\iris.h"
-#include "models\nn.h"
-#include "preproc.h"
-
-#include <chrono>
+#include <iostream>
+#include <windows.h>
 
 int main()
 {
-    iris data = load_iris();
-    Ten features = data.features;
-    Ten targets = data.targets;
+    SetConsoleOutputCP(CP_UTF8);
 
-    targets = one_hot(targets, 3);
+    EnEs en_es = load_en_es();
 
-    TrainTest train_temp = train_test_split(features, targets, 0.2, 42);
-    TrainTest val_test = train_test_split(train_temp.test_features, train_temp.test_targets, 0.5, 42);
-
-    train_temp.train_features = min_max_scaler(train_temp.train_features);
-    val_test.train_features = min_max_scaler(val_test.train_features);
-    val_test.test_features = min_max_scaler(val_test.test_features);
-
-    nn classifier = nn({4, 128, 3}, 0.01f);
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    classifier.train(train_temp.train_features, train_temp.train_targets, val_test.train_features,
-                     val_test.train_targets);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-
-    std::cout << "Time taken: " << duration.count() << " seconds\n";
-
-    classifier.pred(val_test.test_features, val_test.test_targets);
+    for (int i = 0; i < en_es.targetRaw.size(); ++i)
+        std::cout << en_es.targetRaw[i] << " " << en_es.contextRaw[i] << std::endl;
 
     return 0;
 }
