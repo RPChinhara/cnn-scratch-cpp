09a5ae767fcdc827d1357b8b9177c7309eb423d8
kimi <heipo_nogu@outlook.jp>
2024-04-07 08:52:38 +0900

    finally this is it for refactorization?

--- demos/nn.cpp
+++ demos/nn.cpp
@@ -13,25 +13,24 @@ int main()
     y = one_hot(y, 3);
 
     train_test tr_te = train_test_split(x, y, 0.2, 42);
-    train_test v_te = train_test_split(tr_te.test_features, tr_te.test_targets, 0.5, 42);
+    train_test v_te = train_test_split(tr_te.x_test, tr_te.y_test, 0.5, 42);
 
-    tr_te.train_features = min_max_scaler(tr_te.train_features);
-    v_te.train_features = min_max_scaler(v_te.train_features);
-    v_te.test_features = min_max_scaler(v_te.test_features);
+    tr_te.x_train = min_max_scaler(tr_te.x_train);
+    v_te.x_train = min_max_scaler(v_te.x_train);
+    v_te.x_test = min_max_scaler(v_te.x_test);
 
     nn classifier = nn({4, 128, 3}, {ACT_RELU, ACT_SOFTMAX}, 0.01f);
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    classifier.train(tr_te.train_features, tr_te.train_targets, v_te.train_features,
-                     v_te.train_targets);
+    classifier.train(tr_te.x_train, tr_te.y_train, v_te.x_train, v_te.y_train);
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
     std::cout << "Time taken: " << duration.count() << " seconds\n";
 
-    classifier.pred(v_te.test_features, v_te.test_targets);
+    classifier.pred(v_te.x_test, v_te.y_test);
 
     return 0;
 }
