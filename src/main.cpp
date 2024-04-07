35ad2e843e6561b74595a9078d91872b1f100342
kimi <heipo_nogu@outlook.jp>
2024-04-08 07:54:36 +0900

    back to blog please

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,16 @@
-// #include "act.h"
-// #include "datas/enes.h"
-// #include "mdls/trans.h"
-// #include "ten.h"
+#include "datas/enes.h"
 
-// #include <iostream>
-// #include <windows.h>
-
-// int main()
-// {
-//     SetConsoleOutputCP(CP_UTF8);
-
-//     EnEs en_es = load_en_es();
-
-//     // for (int i = 0; i < en_es.targetRaw.size(); ++i)
-//     //     std::cout << en_es.targetRaw[i] << " " << en_es.contextRaw[i] << std::endl;
-
-//     // x = np.array([ 2.0, 1.0, 0.1 ])
-
-//     ten x = ten({2.0, 1.0, 0.1}, {1, 3});
-//     std::cout << act(x, ACT_SOFTMAX, DEV_CPU) << std::endl;
-
-//     return 0;
-// }
-
-#include "datas/iris.h"
-#include "mdls/nn.h"
-#include "preproc.h"
-
-#include <chrono>
+#include <iostream>
+#include <windows.h>
 
 int main()
 {
-    iris data = load_iris();
-    ten x = data.x;
-    ten y = data.y;
+    SetConsoleOutputCP(CP_UTF8);
 
-    y = one_hot(y, 3);
+    EnEs en_es = load_en_es();
 
-    train_test tr_te = train_test_split(x, y, 0.2, 42);
-    train_test v_te = train_test_split(tr_te.x_test, tr_te.y_test, 0.5, 42);
-
-    tr_te.x_train = min_max_scaler(tr_te.x_train);
-    v_te.x_train = min_max_scaler(v_te.x_train);
-    v_te.x_test = min_max_scaler(v_te.x_test);
-
-    nn classifier = nn({4, 64, 32, 3}, {ACT_RELU, ACT_RELU, ACT_SOFTMAX}, 0.01f);
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    classifier.train(tr_te.x_train, tr_te.y_train, v_te.x_train, v_te.y_train);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-
-    std::cout << "Time taken: " << duration.count() << " seconds\n";
-
-    classifier.pred(v_te.x_test, v_te.y_test);
+    for (int i = 0; i < en_es.targetRaw.size(); ++i)
+        std::cout << en_es.targetRaw[i] << " " << en_es.contextRaw[i] << std::endl;
 
     return 0;
 }
