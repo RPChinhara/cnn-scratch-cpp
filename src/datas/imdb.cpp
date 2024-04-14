5530c3ca78d6453c516038947b06572f83ccf287
mon <yukawa_hidenori@icloud.com>
2024-04-15 04:42:59 +0900

    use snake cases

--- src/datas/imdb.cpp
+++ src/datas/imdb.cpp
@@ -42,25 +42,25 @@ IMDB LoadIMDB()
         }
 
         std::string text = line.substr(startPos, endPos - startPos);
-        std::string textNoLink = regex_replace(text, R"((https?:\/\/|www\.)\S+)", "");
-        std::string textNoHTML = regex_replace(textNoLink, "<[^>]*>", " ");
-        std::string textSpacesBetweenPunc = regex_replace(textNoHTML, "([.,!?-])", " $1 ");
-        std::string text_no_consecutive_spaces = regex_replace(textSpacesBetweenPunc, "\\s{2,}", " ");
-        std::string textNoPunc = RemovePunct(text_no_consecutive_spaces);
-        std::string textNoNumber = RemoveNumber(textNoPunc);
-        std::string textNoASCII = RemoveNonASCII(textNoNumber);
-        std::string textNoWhiteSpace = RemoveWhiteSpace(textNoASCII);
-        std::string textNoEmoji = RemoveEmoji(textNoWhiteSpace);
-        std::string textSpellCorrected = SpellCorrection(textNoEmoji);
+        std::string text_no_link = regex_replace(text, R"((https?:\/\/|www\.)\S+)", "");
+        std::string text_no_html = regex_replace(text_no_link, "<[^>]*>", " ");
+        std::string text_sp_around_punc = regex_replace(text_no_html, "([.,!?-])", " $1 ");
+        std::string text_no_consecutive_sp = regex_replace(text_sp_around_punc, "\\s{2,}", " ");
+        std::string text_no_punc = RemovePunct(text_no_consecutive_sp);
+        std::string text_no_num = RemoveNumber(text_no_punc);
+        std::string text_no_ascii = RemoveNonASCII(text_no_num);
+        std::string text_no_white_sp = RemoveWhiteSpace(text_no_ascii);
+        std::string text_no_emoji = RemoveEmoji(text_no_white_sp);
+        std::string text_spell_corrected = SpellCorrection(text_no_emoji);
 
-        auto tokens = Tokenizer(textSpellCorrected);
-        auto tokensNoStopWords = RemoveStopWords(tokens);
+        auto tokens = Tokenizer(text_spell_corrected);
+        auto tokens_no_stop_words = RemoveStopWords(tokens);
 
         std::cout << "Text: " << idx + 1 << std::endl;
         std::cout << "++++++++++++++++++++++++++: " << std::endl;
-        for (int i = 0; i < tokensNoStopWords.size(); ++i)
+        for (int i = 0; i < tokens_no_stop_words.size(); ++i)
         {
-            std::cout << tokensNoStopWords[i] << std::endl;
+            std::cout << tokens_no_stop_words[i] << std::endl;
         }
         std::cout << "--------------------------: " << sentiments[idx] << std::endl << std::endl;
         ++idx;
