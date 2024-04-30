66e11a7152375c213cdd028b37552d14249930fb
kimi <heipo_nogu@outlook.jp>
2024-04-30 20:45:34 +0900

    fuck examples or demos

--- src/cnn2d.h
+++ src/cnn2d.h
@@ -17,3 +17,33 @@ class CNN2D
     std::vector<size_t> filters;
     float learning_rate;
 };
+
+/*
+#include "cnn2d.h"
+#include "mnist.h"
+#include "preproc.h"
+
+int main()
+{
+    MNIST mnist = LoadMNIST();
+
+    for (auto i = 0; i < 784; ++i)
+    {
+
+        if (i % 28 == 0)
+            std::cout << std::endl;
+        std::cout << mnist.trainImages[i] << "   ";
+    }
+
+    mnist.trainImages / 255.0f;
+    mnist.testImages / 255.0f;
+
+    mnist.trainLabels = OneHot(mnist.trainLabels, 10);
+    mnist.testLabels = OneHot(mnist.testLabels, 10);
+
+    CNN2D cnn2D = CNN2D({3, 128, 3}, 0.01f);
+    cnn2D.Train(mnist.trainImages, mnist.trainLabels, mnist.testImages, mnist.testLabels);
+
+    return 0;
+}
+*/
--- src/nn.h
+++ src/nn.h
@@ -29,3 +29,42 @@ class nn
     float mom = 0.1f;
     size_t patience = 4;
 };
+
+/*
+#include "iris.h"
+#include "nn.h"
+#include "preproc.h"
+
+#include <chrono>
+
+int main()
+{
+    iris data = load_iris();
+    ten x = data.x;
+    ten y = data.y;
+
+    y = one_hot(y, 3);
+
+    train_test tr_te = train_test_split(x, y, 0.2, 42);
+    train_test v_te = train_test_split(tr_te.x_test, tr_te.y_test, 0.5, 42);
+
+    tr_te.x_train = min_max_scaler(tr_te.x_train);
+    v_te.x_train = min_max_scaler(v_te.x_train);
+    v_te.x_test = min_max_scaler(v_te.x_test);
+
+    nn classifier = nn({4, 64, 64, 3}, {RELU, RELU, SOFTMAX}, 0.01f);
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    classifier.train(tr_te.x_train, tr_te.y_train, v_te.x_train, v_te.y_train);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+
+    std::cout << "Time taken: " << duration.count() << " seconds\n";
+
+    classifier.pred(v_te.x_test, v_te.y_test);
+
+    return 0;
+}
+*/
