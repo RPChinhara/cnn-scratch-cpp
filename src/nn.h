11cd72b50a8abdeee31e9f905ced01ad9742b331
kimi <heipo_nogu@outlook.jp>
2024-04-30 20:52:20 +0900

    call them train_test and val_test

--- src/nn.h
+++ src/nn.h
@@ -45,25 +45,25 @@ int main()
 
     y = one_hot(y, 3);
 
-    train_test tr_te = train_test_split(x, y, 0.2, 42);
-    train_test v_te = train_test_split(tr_te.x_test, tr_te.y_test, 0.5, 42);
+    train_test train_test = train_test_split(x, y, 0.2, 42);
+    train_test val_test = train_test_split(train_test.x_test, train_test.y_test, 0.5, 42);
 
-    tr_te.x_train = min_max_scaler(tr_te.x_train);
-    v_te.x_train = min_max_scaler(v_te.x_train);
-    v_te.x_test = min_max_scaler(v_te.x_test);
+    train_test.x_train = min_max_scaler(train_test.x_train);
+    val_test.x_train = min_max_scaler(val_test.x_train);
+    val_test.x_test = min_max_scaler(val_test.x_test);
 
     nn classifier = nn({4, 64, 64, 3}, {RELU, RELU, SOFTMAX}, 0.01f);
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    classifier.train(tr_te.x_train, tr_te.y_train, v_te.x_train, v_te.y_train);
+    classifier.train(train_test.x_train, train_test.y_train, val_test.x_train, val_test.y_train);
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
     std::cout << "Time taken: " << duration.count() << " seconds\n";
 
-    classifier.pred(v_te.x_test, v_te.y_test);
+    classifier.pred(val_test.x_test, val_test.y_test);
 
     return 0;
 }
