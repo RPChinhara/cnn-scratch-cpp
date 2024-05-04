66e342b8fcc76debb5829eb75b81dd9f75ec894b
kimi <heipo_nogu@outlook.jp>
2024-05-05 03:12:13 +0900

    just t1 and t2 for kernels too

--- src/knls.cu
+++ src/knls.cu
@@ -16,20 +16,20 @@ __global__ void Log(float *t, float *t_new, size_t n)
         t_new[id] = logf(t[id]);
 }
 
-__global__ void matmul(float *t_1, float *t_2, float *t_new, size_t num_rows_t_1, size_t num_cols_t_1,
-                       size_t num_rows_t_2)
+__global__ void matmul(float *t1, float *t2, float *t_new, size_t num_rows_t1, size_t num_cols_t1,
+                       size_t num_rows_t2)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (i < num_rows_t_1 && j < num_rows_t_2)
+    if (i < num_rows_t1 && j < num_rows_t2)
     {
         float sum = 0.0;
 
-        for (auto l = 0; l < num_cols_t_1; l++)
-            sum += t_1[i * num_cols_t_1 + l] * t_2[l * num_rows_t_2 + j];
+        for (auto l = 0; l < num_cols_t1; l++)
+            sum += t1[i * num_cols_t1 + l] * t2[l * num_rows_t2 + j];
 
-        t_new[i * num_rows_t_2 + j] = sum;
+        t_new[i * num_rows_t2 + j] = sum;
     }
 }
 
