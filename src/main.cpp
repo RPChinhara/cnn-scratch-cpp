f66940944a45c6b4056996f4c0c433ea7cdd7f43
kimi <heipo_nogu@outlook.jp>
2024-05-05 06:56:26 +0900

    remove parenthesses

--- src/main.cpp
+++ src/main.cpp
@@ -12,28 +12,28 @@ int main()
 
     for (auto i = 0; i < x.size(); ++i)
     {
-        x[i] = regex_replace(data.x[i], L"(á)", L"a");
-        x[i] = regex_replace(x[i], L"(é)", L"e");
-        x[i] = regex_replace(x[i], L"(í)", L"i");
-        x[i] = regex_replace(x[i], L"(ó)", L"o");
-        x[i] = regex_replace(x[i], L"(ú)", L"u");
+        x[i] = regex_replace(data.x[i], L"á", L"a");
+        x[i] = regex_replace(x[i], L"é", L"e");
+        x[i] = regex_replace(x[i], L"í", L"i");
+        x[i] = regex_replace(x[i], L"ó", L"o");
+        x[i] = regex_replace(x[i], L"ú", L"u");
+        
+        x[i] = regex_replace(x[i], L"Á", L"A");
+        x[i] = regex_replace(x[i], L"É", L"E");
+        x[i] = regex_replace(x[i], L"Í", L"I");
+        x[i] = regex_replace(x[i], L"Ó", L"O");
+        x[i] = regex_replace(x[i], L"Ú", L"U");
 
-        x[i] = regex_replace(x[i], L"(Á)", L"A");
-        x[i] = regex_replace(x[i], L"(É)", L"E");
-        x[i] = regex_replace(x[i], L"(Í)", L"I");
-        x[i] = regex_replace(x[i], L"(Ó)", L"O");
-        x[i] = regex_replace(x[i], L"(Ú)", L"U");
-
-        x[i] = regex_replace(x[i], L"(ñ)", L"n");
-        x[i] = regex_replace(x[i], L"(Ñ)", L"N");
-        x[i] = regex_replace(x[i], L"(ü)", L"u");
-        x[i] = regex_replace(x[i], L"(Ü)", L"U");
+        x[i] = regex_replace(x[i], L"ñ", L"n");
+        x[i] = regex_replace(x[i], L"Ñ", L"N");
+        x[i] = regex_replace(x[i], L"ü", L"u");
+        x[i] = regex_replace(x[i], L"Ü", L"U");
 
         x[i] = lower(x[i]);
         y[i] = lower(data.y[i]);
 
-        x[i] = regex_replace(x[i], L"([^ a-z.?!,¿])", L"");
-        y[i] = regex_replace(y[i], L"([^ a-z.?!,¿])", L"");
+        x[i] = regex_replace(x[i], L"[^ a-z.?!,¿]", L"");
+        y[i] = regex_replace(y[i], L"[^ a-z.?!,¿]", L"");
 
         x[i] = regex_replace(x[i], L"([.?!,¿])", L" $1 ");
         y[i] = regex_replace(y[i], L"([.?!,¿])", L" $1 ");
@@ -65,7 +65,7 @@ int main()
     std::vector<std::wstring> in_2 = {{L"foo qux bar"}, {L"qux baz dog sex"}, {L"qux baz dog sex"}};
 
     auto z = text_vectorization(vocab, in_2, 9);
-    
+
     std::cout << z << std::endl;
 
     return 0;
