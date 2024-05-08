f89230db6f7e7ccd8fe367d4d600a696331422e4
KKK <heipo_nogu@outlook.jp>
2024-05-08 09:23:28 +0900

    move nn and cdd to layers files

--- src/layers.cpp
+++ src/layers.cpp
@@ -1,4 +1,3 @@
-#include "nn.h"
 #include "arrs.h"
 #include "dev.h"
 #include "diffs.h"
@@ -6,6 +5,7 @@
 #include "losses.h"
 #include "math.hpp"
 #include "mets.h"
+#include "nn.h"
 #include "rd.h"
 #include "ten.h"
 
@@ -188,4 +188,50 @@ std::vector<ten> nn::forward_prop(const ten &x, const std::vector<ten> &w, const
     }
 
     return a;
+}
+
+cnn2d::cnn2d(const std::vector<size_t> &filters, float const learning_rate)
+{
+    this->filters = filters;
+    this->learning_rate = learning_rate;
+}
+
+void cnn2d::Train(const ten &xTrain, const ten &yTrain, const ten &xVal, const ten &yVal)
+{
+    // ten kernel = zeros({3, 3});
+    ten kernel = ten({1, -1, 1, 0, 1, 0, -1, 0, 1}, {3, 3});
+
+    size_t kernelHeight = kernel.shape.front();
+    size_t kernelWidth = kernel.shape.back();
+
+    size_t inputHeight = xTrain.shape[1];
+    size_t inputWidth = xTrain.shape[2];
+
+    size_t outputHeight = inputHeight - kernelHeight + 1;
+    size_t outputWidth = inputWidth - kernelWidth + 1;
+
+    ten output = zeros({outputHeight, outputWidth});
+
+    // size_t idx = 0;
+
+    // for (auto i = 0; i < outputHeight; ++i)
+    // {
+    //     for (auto j = 0; i < outputWidth; ++j)
+    //     {
+    //         // ouput[idx] =
+    //     }
+    // }
+
+    // std::cout << output << std::endl;
+}
+
+void cnn2d::Predict(const ten &xTest, const ten &yTest)
+{
+}
+
+std::vector<ten> cnn2d::ForwardPropagation(const ten &input, const std::vector<ten> &kernel, const size_t stride)
+{
+    std::vector<ten> weights;
+
+    return weights;
 }
