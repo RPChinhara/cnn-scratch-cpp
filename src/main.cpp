3fa2008fc9f25a5c62a374e655d58809a77271d3
kimi <heipo_nogu@outlook.jp>
2024-05-18 00:42:59 +0900

    rm comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ int main()
 {
     std::string text = "This is GeeksforGeeks a software training institute";
 
-    // Use std::sort to sort the string in alphabetical order
     std::sort(text.begin(), text.end());
 
     std::set<char> seen;
@@ -20,26 +19,19 @@ int main()
         if (seen.find(ch) == seen.end())
         {
             seen.insert(ch);
-            unique_text += ch; // Append the original character
+            unique_text += ch;
         }
     }
 
     std::cout << unique_text << std::endl;
 
-    // Vector to store character-integer pairs
     std::vector<std::pair<char, int>> char_to_index;
 
-    // Assign integer to each character
     for (auto i = 0; i < unique_text.size(); ++i)
-    {
         char_to_index.emplace_back(unique_text[i], i);
-    }
 
-    // Output the character-integer pairs
     for (const auto &pair : char_to_index)
-    {
         std::cout << "'" << pair.first << "' -> " << pair.second << std::endl;
-    }
-    
+
     return 0;
 }
