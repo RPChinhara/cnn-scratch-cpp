775ac35fafe714b8e268b6f14ff2a4df47861349
kimi <heipo_nogu@outlook.jp>
2024-06-06 16:38:08 +0900

    almost done with preprocessing imdb?

--- src/datas.cpp
+++ src/datas.cpp
@@ -78,7 +78,6 @@ imdb load_imdb()
 
         std::string text = line.substr(startPos, endPos - startPos);
         reviews.push_back(text);
-
     }
 
     file.close();
@@ -88,11 +87,11 @@ imdb load_imdb()
         reviews[i] = lower(reviews[i]);
         reviews[i] = regex_replace(reviews[i], R"((https?:\/\/|www\.)\S+)", "");
         reviews[i] = regex_replace(reviews[i], "<[^>]*>", " ");
-        reviews[i] = regex_replace(reviews[i], "([.,!?-])", " $1 ");
         reviews[i] = regex_replace(reviews[i], "\\s{2,}", " ");
-        reviews[i] = regex_replace(reviews[i], "[\"#$%&'()*+/:;<=>@\\[\\\\\\]^_`{|}~]", " ");
-        reviews[i] = regex_replace(reviews[i], "[^\\x00-\\x7f]", " ");
         reviews[i] = regex_replace(reviews[i], "\\s+", " ");
+        reviews[i] = regex_replace(reviews[i], "\"", "");
+        reviews[i] = regex_replace(reviews[i], "[\".,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
+        reviews[i] = regex_replace(reviews[i], "[^\\x00-\\x7f]", " ");
         reviews[i] = regex_replace(reviews[i], "[\xE2\x98\x80-\xE2\x9B\xBF]", "");
         reviews[i] = regex_replace(reviews[i], "(.)\\1+", "$1$1");
     }
