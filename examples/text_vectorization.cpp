547aece9cd585577ff37302ef737d8e45f1ef14a
kimi <heipo_nogu@outlook.jp>
2024-06-19 05:46:42 +0900

    make example file for text_vectorization()

---
+++ examples/text_vectorization.cpp
@@ -0,0 +1,16 @@
+#include "lyrs.h"
+
+#include <iostream>
+
+int main()
+{
+    const size_t max_tokens = 5000;
+    const size_t max_len = 4;
+
+    std::vector<std::string> vocab = {"foo bar", "bar baz", "baz bada boom"};
+    std::vector<std::string> in = {"foo qux bar", "qux baz"};
+
+    std::cout << text_vectorization(vocab, in, max_tokens, max_len) << std::endl;
+
+    return 0;
+}
