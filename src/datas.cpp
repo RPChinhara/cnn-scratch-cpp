f22661090e2d8371e35dcaf721acc7e88a8fd496
KKK <heipo_nogu@outlook.jp>
2024-06-24 07:32:15 +0900

    house keeping

--- src/datas.cpp
+++ src/datas.cpp
@@ -120,8 +120,9 @@ iris load_iris()
     size_t idx_x = 0;
     size_t idx_y = 0;
 
-    ten x = zeros({150, 4});
-    ten y = zeros({150, 1});
+    iris data;
+    data.x = zeros({150, 4});
+    data.y = zeros({150, 1});
 
     std::string line;
     std::getline(file, line);
@@ -134,45 +135,41 @@ iris load_iris()
         std::getline(ss, value, ',');
 
         std::getline(ss, value, ',');
-        x[idx_x] = std::stof(value);
+        data.x[idx_x] = std::stof(value);
         ++idx_x;
 
         std::getline(ss, value, ',');
-        x[idx_x] = std::stof(value);
+        data.x[idx_x] = std::stof(value);
         ++idx_x;
 
         std::getline(ss, value, ',');
-        x[idx_x] = std::stof(value);
+        data.x[idx_x] = std::stof(value);
         ++idx_x;
 
         std::getline(ss, value, ',');
-        x[idx_x] = std::stof(value);
+        data.x[idx_x] = std::stof(value);
         ++idx_x;
 
         std::getline(ss, value);
 
         if (value == "Iris-setosa")
         {
-            y[idx_y] = 0.0f;
+            data.y[idx_y] = 0.0f;
             ++idx_y;
         }
         else if (value == "Iris-versicolor")
         {
-            y[idx_y] = 1.0f;
+            data.y[idx_y] = 1.0f;
             ++idx_y;
         }
         else if (value == "Iris-virginica")
         {
-            y[idx_y] = 2.0f;
+            data.y[idx_y] = 2.0f;
             ++idx_y;
         }
     }
 
     file.close();
-
-    iris data;
-    data.x = x;
-    data.y = y;
 
     return data;
 }
