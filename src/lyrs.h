679bd1e42327435809f7aa7e81320cee05ab5121
KKK <heipo_nogu@outlook.jp>
2024-06-25 00:46:39 +0900

    comment code to print vocab up to 200

--- src/lyrs.h
+++ src/lyrs.h
@@ -133,6 +133,10 @@ ten text_vectorization(const std::vector<T> &vocab, const std::vector<T> &in, si
     vocab_vec.insert(vocab_vec.begin(), std::pair<T, float>("[UNK]", 1.0f));
     vocab_vec.insert(vocab_vec.begin(), std::pair<T, float>("", 0.0f));
 
+    // size_t num_vocab = 200;
+    // for (auto i = 0; i < num_vocab; ++i)
+    //     std::cout << i << ": " << vocab_vec[i].first << " " << vocab_vec[i].second << std::endl;
+
     ten t_new = zeros({in.size(), max_len});
 
     size_t idx = 0;
