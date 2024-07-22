4612d07219a21ce330a588ad4582417ee7cca493
kimi <heipo_nogu@outlook.jp>
2024-07-23 07:20:17 +0900

    use << operator instead

--- src/lyrs.cpp
+++ src/lyrs.cpp
@@ -292,13 +292,12 @@ void rnn::train(const ten &x_train, const ten &y_train, const ten &x_val,
             std::chrono::duration_cast<std::chrono::seconds>(duration);
         auto remaining_ms = duration - seconds;
 
-        std::cout << "Epoch " + std::to_string(i) + "/" +
-                         std::to_string(epochs) + "\n" +
-                         std::to_string(seconds.count()) + "s " +
-                         std::to_string(remaining_ms.count()) +
-                         "ms/step - loss: " +
-                         std::to_string(
-                             categorical_cross_entropy(y_train, a.back()))
+        std::cout << "Epoch " << std::to_string(i) << "/"
+                  << std::to_string(epochs) << "\n"
+                  << std::to_string(seconds.count()) << "s "
+                  << std::to_string(remaining_ms.count()) << "ms/step - loss: "
+                  << std::to_string(
+                         categorical_cross_entropy(y_train, a.back()))
                   << std::endl;
     }
 }
