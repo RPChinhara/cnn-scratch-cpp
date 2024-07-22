874c647864c0ccaf97c9350eb8443aaae89b8082
kimi <heipo_nogu@outlook.jp>
2024-07-23 03:15:21 +0900

    add an epoch as member var

--- src/lyrs.h
+++ src/lyrs.h
@@ -18,11 +18,13 @@ class cnn2d
     std::vector<size_t> filters;
     float lr;
 
-    std::vector<ten> forward(const ten &input, const std::vector<ten> &kernel, const size_t stride);
+    std::vector<ten> forward(const ten &input, const std::vector<ten> &kernel,
+                             const size_t stride);
 
   public:
     cnn2d(const std::vector<size_t> &filters, float const lr);
-    void train(const ten &xTrain, const ten &yTrain, const ten &xVal, const ten &yVal);
+    void train(const ten &xTrain, const ten &yTrain, const ten &xVal,
+               const ten &yVal);
     void pred(const ten &xTest, const ten &yTest);
 };
 
@@ -71,11 +73,14 @@ class nn
     std::pair<std::vector<ten>, std::vector<ten>> w_b_mom;
 
     std::pair<std::vector<ten>, std::vector<ten>> init_params();
-    std::vector<ten> forward(const ten &x, const std::vector<ten> &w, const std::vector<ten> &b);
+    std::vector<ten> forward(const ten &x, const std::vector<ten> &w,
+                             const std::vector<ten> &b);
 
   public:
-    nn(const std::vector<size_t> &lyrs, const std::vector<act_type> &act_types, float const lr);
-    void train(const ten &x_train, const ten &y_train, const ten &x_val, const ten &y_val);
+    nn(const std::vector<size_t> &lyrs, const std::vector<act_type> &act_types,
+       float const lr);
+    void train(const ten &x_train, const ten &y_train, const ten &x_val,
+               const ten &y_val);
     void pred(const ten &x_test, const ten &y_test);
 };
 
@@ -84,6 +89,7 @@ class rnn
   private:
     float lr;
     size_t batch_size = 8316;
+    size_t epochs = 10;
 
     size_t in_size = 1;
     size_t hidden_size = 50;
@@ -100,13 +106,15 @@ class rnn
 
   public:
     rnn(const size_t lr);
-    void train(const ten &x_train, const ten &y_train, const ten &x_val, const ten &y_val);
+    void train(const ten &x_train, const ten &y_train, const ten &x_val,
+               const ten &y_val);
 };
 
 ten embedding(const size_t vocab_size, const size_t cols, const ten &ind);
 
 template <typename T>
-ten text_vectorization(const std::vector<T> &vocab, const std::vector<T> &in, size_t max_tokens, const size_t max_len)
+ten text_vectorization(const std::vector<T> &vocab, const std::vector<T> &in,
+                       size_t max_tokens, const size_t max_len)
 {
     assert(max_tokens > 2);
 
@@ -125,21 +133,24 @@ ten text_vectorization(const std::vector<T> &vocab, const std::vector<T> &in, si
         }
     }
 
-    std::vector<std::pair<T, float>> vocab_vec(vocab_map.begin(), vocab_map.end());
+    std::vector<std::pair<T, float>> vocab_vec(vocab_map.begin(),
+                                               vocab_map.end());
 
-    std::sort(vocab_vec.begin(), vocab_vec.end(), [](const std::pair<T, float> &a, const std::pair<T, float> &b) {
-        if (a.second != b.second)
-            return a.second > b.second;
-        else
-            return a.first > b.first;
-    });
+    std::sort(vocab_vec.begin(), vocab_vec.end(),
+              [](const std::pair<T, float> &a, const std::pair<T, float> &b) {
+                  if (a.second != b.second)
+                      return a.second > b.second;
+                  else
+                      return a.first > b.first;
+              });
 
     vocab_vec.insert(vocab_vec.begin(), std::pair<T, float>("[UNK]", 1.0f));
     vocab_vec.insert(vocab_vec.begin(), std::pair<T, float>("", 0.0f));
 
     // size_t num_vocab = 200;
     // for (auto i = 0; i < num_vocab; ++i)
-    //     std::cout << i << ": " << vocab_vec[i].first << " " << vocab_vec[i].second << std::endl;
+    //     std::cout << i << ": " << vocab_vec[i].first << " " <<
+    //     vocab_vec[i].second << std::endl;
 
     ten t_new = zeros({in.size(), max_len});
 
