fba73e4d0ab9e1be23145b8ad05438210dfce58b
kimi <heipo_nogu@outlook.jp>
2024-07-29 13:59:33 +0900

    change style

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,12 @@
 #include "lyrs.h"
 #include "preproc.h"
 
-std::pair<ten, ten> create_sequences(const ten &data, const size_t seq_length)
-{
+std::pair<ten, ten> create_sequences(const ten &data, const size_t seq_length) {
     ten x = zeros({data.size - seq_length - 1, seq_length, 1});
     ten y = zeros({data.size - seq_length - 1, 1});
 
     size_t idx = 0;
-    for (auto i = 0; i < (data.size - seq_length - 1) * seq_length; ++i)
-    {
+    for (auto i = 0; i < (data.size - seq_length - 1) * seq_length; ++i) {
         if (i % seq_length == 0 && i != 0)
             idx -= seq_length - 1;
         x[i] = data[idx];
@@ -23,8 +21,7 @@ std::pair<ten, ten> create_sequences(const ten &data, const size_t seq_length)
     return std::make_pair(x, y);
 }
 
-int main()
-{
+int main() {
     const float test_size = 0.2f;
     const size_t seq_length = 10;
     const size_t lr = 0.01f;
