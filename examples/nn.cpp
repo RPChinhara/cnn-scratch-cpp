182cdfdf3e3cfbe6e2a491119c24e101e13a62dd
kimi <heipo_nogu@outlook.jp>
2024-07-31 22:01:01 +0900

    move to losses

--- examples/nn.cpp
+++ examples/nn.cpp
@@ -1,22 +1,10 @@
 #include "datas.h"
 #include "lyrs.h"
 #include "preproc.h"
+#include "losses.h"
 #include "math.hpp"
 
 #include <chrono>
-
-float categorical_cross_entropy(const ten &y_true, const ten &y_pred) {
-    float sum = 0.0f;
-    constexpr float epsilon = 1e-15f;
-    size_t num_samples = y_true.shape.front();
-    ten y_pred_clipped = clip_by_value(y_pred, epsilon, 1.0f - epsilon);
-    ten y_pred_logged = log(y_pred_clipped, CPU);
-
-    for (auto i = 0; i < y_true.size; ++i)
-        sum += y_true[i] * y_pred_logged[i];
-
-    return -sum / num_samples;
-}
 
 float categorical_accuracy(const ten &y_true, const ten &y_pred) {
     ten idx_true = argmax(y_true);
