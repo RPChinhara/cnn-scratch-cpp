4b10abb41d9717638d8cdb6cdf69e2943d9bad6c
kimi <heipo_nogu@outlook.jp>
2024-07-31 22:20:53 +0900

    rm losses.cpp

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 set NVCC=nvcc
 set FLAGS=-arch sm_75 -I ..\src -o dora --optimize 3 -std c++20 --threads 0 --use_fast_math -Xcompiler /GL -Xcompiler /MP -Xcompiler /O2 -Xcompiler /utf-8
-set SRC=..\src\act.cu ..\src\arrs.cpp ..\src\datas.cpp ..\src\diffs.cpp ..\src\knls.cu ..\src\linalg.cu ..\src\losses.cpp ..\src\lyrs.cpp ..\src\main.cpp ..\src\math.cu ..\src\preproc.cpp ..\src\rd.cpp ..\src\ten.cpp
+set SRC=..\src\act.cu ..\src\arrs.cpp ..\src\datas.cpp ..\src\diffs.cpp ..\src\knls.cu ..\src\linalg.cu ..\src\lyrs.cpp ..\src\main.cpp ..\src\math.cu ..\src\preproc.cpp ..\src\rd.cpp ..\src\ten.cpp
 
 if not exist bin mkdir bin
 
