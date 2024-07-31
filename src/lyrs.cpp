33da789f51ae1a7e62870df2c9789b709783f147
kimi <heipo_nogu@outlook.jp>
2024-07-31 14:23:43 +0900

    use mse instead

--- src/lyrs.cpp
+++ src/lyrs.cpp
@@ -231,7 +231,7 @@ void rnn::train(const ten &x_train, const ten &y_train, const ten &x_val, const 
         auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
         auto remaining_ms = duration - seconds;
 
-        std::cout << "Epoch " << i << "/" << epochs << std::endl << seconds.count() << "s " << remaining_ms.count() << "ms/step - loss: " << categorical_cross_entropy(y_train, a.back()) << std::endl;
+        std::cout << "Epoch " << i << "/" << epochs << std::endl << seconds.count() << "s " << remaining_ms.count() << "ms/step - loss: " << mse(y_train, a.back()) << std::endl;
     }
 }
 
