a07aea94331aa91aeb989d9622a856d5a267111a
kimi <heipo_nogu@outlook.jp>
2024-07-31 14:00:14 +0900

    house keeping

--- src/lyrs.h
+++ src/lyrs.h
@@ -12,24 +12,20 @@
 
 class ten;
 
-class cnn2d
-{
+class cnn2d {
   private:
     std::vector<size_t> filters;
     float lr;
 
-    std::vector<ten> forward(const ten &input, const std::vector<ten> &kernel,
-                             const size_t stride);
+    std::vector<ten> forward(const ten &input, const std::vector<ten> &kernel, const size_t stride);
 
   public:
     cnn2d(const std::vector<size_t> &filters, float const lr);
-    void train(const ten &xTrain, const ten &yTrain, const ten &xVal,
-               const ten &yVal);
+    void train(const ten &xTrain, const ten &yTrain, const ten &xVal, const ten &yVal);
     void pred(const ten &xTest, const ten &yTest);
 };
 
-class gru
-{
+class gru {
   private:
     size_t num_ins = 10;
     size_t num_hiddens = 20;
@@ -56,8 +52,7 @@ class gru
     gru(const size_t units);
 };
 
-class nn
-{
+class nn {
   private:
     std::vector<ten> a;
     std::vector<act_type> act_types;
@@ -73,19 +68,15 @@ class nn
     std::pair<std::vector<ten>, std::vector<ten>> w_b_mom;
 
     std::pair<std::vector<ten>, std::vector<ten>> init_params();
-    std::vector<ten> forward(const ten &x, const std::vector<ten> &w,
-                             const std::vector<ten> &b);
+    std::vector<ten> forward(const ten &x, const std::vector<ten> &w, const std::vector<ten> &b);
 
   public:
-    nn(const std::vector<size_t> &lyrs, const std::vector<act_type> &act_types,
-       float const lr);
-    void train(const ten &x_train, const ten &y_train, const ten &x_val,
-               const ten &y_val);
+    nn(const std::vector<size_t> &lyrs, const std::vector<act_type> &act_types, float const lr);
+    void train(const ten &x_train, const ten &y_train, const ten &x_val, const ten &y_val);
     void pred(const ten &x_test, const ten &y_test);
 };
 
-class rnn
-{
+class rnn {
   private:
     float lr;
     size_t batch_size = 8316;
