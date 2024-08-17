1dde0cb0e70384b8b33675dcae88080d37c8e47d
kimi <heipo_nogu@outlook.jp>
2024-08-18 03:43:40 +0900

    rm act type and switches

--- src/diffs.cpp
+++ src/diffs.cpp
@@ -1,40 +1,22 @@
 #include "diffs.h"
 #include "ten.h"
 
-ten da_dz(const ten &a, act_type act)
-{
+ten da_dz(const ten &a) {
     ten t_new = a;
 
-    switch (act)
+    for (auto i = 0; i < a.size; ++i)
     {
-    case RELU: {
-        for (auto i = 0; i < a.size; ++i)
-        {
-            if (0.0f < a[i])
-                t_new[i] = 1.0f;
-            else if (a[i] == 0.0f)
-                t_new[i] = 0.0f;
-            else
-                t_new[i] = 0.0f;
-        }
+        if (0.0f < a[i])
+            t_new[i] = 1.0f;
+        else if (a[i] == 0.0f)
+            t_new[i] = 0.0f;
+        else
+            t_new[i] = 0.0f;
+    }
 
-        return t_new;
-    }
-    default:
-        std::cout << "Unknown act." << std::endl;
-        return ten();
-    }
+    return t_new;
 }
 
-ten dl_da_da_dz(const ten &y_true, const ten &y_pred, act_type act)
-{
-    switch (act)
-    {
-    case SOFTMAX: {
-        return (y_pred - y_true);
-    }
-    default:
-        std::cout << "Unknown act." << std::endl;
-        return ten();
-    }
+ten dl_da_da_dz(const ten &y_true, const ten &y_pred) {
+    return (y_pred - y_true);
 }
