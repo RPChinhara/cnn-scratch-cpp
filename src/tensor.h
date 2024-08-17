3060638e6945c91d45a69b048a9df92e62405fa9
kimi <heipo_nogu@outlook.jp>
2024-08-18 07:06:26 +0900

    same as before

---
+++ src/tensor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+class tensor
+{
+  public:
+    size_t size;
+    std::vector<size_t> shape;
+    float *elem = nullptr;
+
+    tensor() = default;
+    tensor(const std::vector<size_t> &shape, const std::vector<float> &elem);
+    ~tensor();
+    tensor(const tensor &other);
+    tensor(tensor &&other) noexcept;
+    tensor &operator=(const tensor &other);
+    tensor &operator=(tensor &&other) noexcept;
+    tensor operator+(const tensor &other) const;
+    tensor operator-(const tensor &other) const;
+    tensor operator*(const tensor &other) const;
+    tensor operator/(const tensor &other) const;
+    tensor operator+=(const tensor &other) const;
+    float &operator[](const size_t idx) const;
+    friend tensor operator-(const float sca, const tensor &t);
+    friend tensor operator*(const float sca, const tensor &t);
+    friend std::ostream &operator<<(std::ostream &os, const tensor &t);
+};
