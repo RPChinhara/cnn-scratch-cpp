55edbbfb443c8a89139a1c932ae2febf239f5701
mon <yukawa_hidenori@icloud.com>
2024-08-25 17:11:30 +0900

    house keeping

--- src/main.cpp
+++ src/main.cpp
@@ -51,8 +51,6 @@ int main() {
     auto x_y_train = create_sequences(train_test.first, seq_length);
     auto x_y_test = create_sequences(train_test.second, seq_length);
 
-    // std::cout << x_y_train.first << std::endl;
-
     rnn model = rnn(hyperbolic_tangent, mean_squared_error, lr);
     model.train(x_y_train.first, x_y_train.second, x_y_test.first, x_y_test.second);
 
