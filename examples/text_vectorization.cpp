4a68209048c2792743f5d3f6065190e1ebbb672d
mon <yukawa_hidenori@icloud.com>
2024-08-27 14:50:18 +0900

    house keeping

--- examples/text_vectorization.cpp
+++ examples/text_vectorization.cpp
@@ -3,13 +3,10 @@
 #include <iostream>
 
 int main() {
-    const size_t max_tokens = 5000;
-    const size_t max_len = 4;
-
     std::vector<std::string> vocab = {"foo bar", "bar baz", "baz bada boom"};
     std::vector<std::string> in = {"foo qux bar", "qux baz"};
 
-    std::cout << text_vectorization(vocab, in, max_tokens, max_len) << std::endl;
+    std::cout << text_vectorization(vocab, in, 5000, 4) << std::endl;
 
     return 0;
 }
