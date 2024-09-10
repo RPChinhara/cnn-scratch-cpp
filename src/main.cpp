e95d12342cc560ed0a58ed6ad609f0778c758834
mon <yukawa_hidenori@icloud.com>
2024-09-11 03:03:49 +0900

    deviding by proper n

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ float mean_squared_error(const tensor &y_true, const tensor &y_pred) {
     for (auto i = 0; i < y_true.size; ++i)
         sum += std::powf(y_true[i] - y_pred[i], 2.0f);
 
-    return sum / y_true.size;
+    return sum / y_true.shape.back(); // deviding by n which is 8317 in this case
 }
 
 int main() {
