9c868182190f890f98fd65df1c962ace2a42462a
mon <yukawa_hidenori@icloud.com>
2024-10-13 16:43:03 +0900

    house keeping

--- src/rd.cpp
+++ src/rd.cpp
@@ -30,6 +30,23 @@ tensor normal_dist(const std::vector<size_t> &shape, const float mean, const flo
     return t_new;
 }
 
+tensor shuffle(const tensor &t, const size_t rd_state) {
+    tensor t_new = t;
+    std::mt19937 rng(rd_state);
+
+    for (auto i = t.shape.front() - 1; 0 < i; --i) {
+        std::uniform_int_distribution<> dist(0, i);
+        int j = dist(rng);
+
+        for (auto k = 0; k < t.shape.back(); ++k) {
+            float temp = t_new[(t.shape.back() - 1) * i + i + k];
+            t_new[(t.shape.back() - 1) * i + i + k] = t_new[(t.shape.back() - 1) * j + j + k];
+            t_new[(t.shape.back() - 1) * j + j + k] = temp;
+        }
+    }
+    return t_new;
+}
+
 tensor uniform_dist(const std::vector<size_t> &shape, const float min_val, const float max_val) {
     tensor t_new = tensor();
 
@@ -52,22 +69,5 @@ tensor uniform_dist(const std::vector<size_t> &shape, const float min_val, const
     for (auto i = 0; i < t_new.size; ++i)
         t_new[i] = dist(rng);
 
-    return t_new;
-}
-
-tensor shuffle(const tensor &t, const size_t rd_state) {
-    tensor t_new = t;
-    std::mt19937 rng(rd_state);
-
-    for (auto i = t.shape.front() - 1; 0 < i; --i) {
-        std::uniform_int_distribution<> dist(0, i);
-        int j = dist(rng);
-
-        for (auto k = 0; k < t.shape.back(); ++k) {
-            float temp = t_new[(t.shape.back() - 1) * i + i + k];
-            t_new[(t.shape.back() - 1) * i + i + k] = t_new[(t.shape.back() - 1) * j + j + k];
-            t_new[(t.shape.back() - 1) * j + j + k] = temp;
-        }
-    }
     return t_new;
 }
--- src/rd.h
+++ src/rd.h
@@ -5,5 +5,5 @@
 class tensor;
 
 tensor normal_dist(const std::vector<size_t> &shape, const float mean = 0.0f, const float std_dev = 0.05f);
-tensor uniform_dist(const std::vector<size_t> &shape, const float min_val = -0.05f, const float max_val = 0.05f);
 tensor shuffle(const tensor &t, const size_t rd_state);
+tensor uniform_dist(const std::vector<size_t> &shape, const float min_val = -0.05f, const float max_val = 0.05f);
