c14f5f7e1ab9d176512f0c80c05350cee1125ee8
mon <yukawa_hidenori@icloud.com>
2024-10-13 16:41:37 +0900

    house keeping

--- src/rd.cpp
+++ src/rd.cpp
@@ -5,8 +5,7 @@
 #include <numeric>
 #include <random>
 
-tensor normal_dist(const std::vector<size_t> &shape, const float mean, const float std_dev)
-{
+tensor normal_dist(const std::vector<size_t> &shape, const float mean, const float std_dev) {
     tensor t_new = tensor();
 
     for (auto i : shape)
@@ -31,8 +30,7 @@ tensor normal_dist(const std::vector<size_t> &shape, const float mean, const flo
     return t_new;
 }
 
-tensor uniform_dist(const std::vector<size_t> &shape, const float min_val, const float max_val)
-{
+tensor uniform_dist(const std::vector<size_t> &shape, const float min_val, const float max_val) {
     tensor t_new = tensor();
 
     for (auto i : shape)
@@ -57,18 +55,15 @@ tensor uniform_dist(const std::vector<size_t> &shape, const float min_val, const
     return t_new;
 }
 
-tensor shuffle(const tensor &t, const size_t rd_state)
-{
+tensor shuffle(const tensor &t, const size_t rd_state) {
     tensor t_new = t;
     std::mt19937 rng(rd_state);
 
-    for (auto i = t.shape.front() - 1; 0 < i; --i)
-    {
+    for (auto i = t.shape.front() - 1; 0 < i; --i) {
         std::uniform_int_distribution<> dist(0, i);
         int j = dist(rng);
 
-        for (auto k = 0; k < t.shape.back(); ++k)
-        {
+        for (auto k = 0; k < t.shape.back(); ++k) {
             float temp = t_new[(t.shape.back() - 1) * i + i + k];
             t_new[(t.shape.back() - 1) * i + i + k] = t_new[(t.shape.back() - 1) * j + j + k];
             t_new[(t.shape.back() - 1) * j + j + k] = temp;
