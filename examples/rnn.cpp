bd75bea27edea39271d7561bfafbd28a9ca582a0
kimi <heipo_nogu@outlook.jp>
2024-10-15 19:10:43 +0900

    upd rnn.cpp

--- examples/rnn.cpp
+++ examples/rnn.cpp
@@ -4,39 +4,40 @@
 #include "preproc.h"
 
 std::pair<tensor, tensor> create_sequences(const tensor &data, const size_t seq_length) {
-    tensor x = zeros({data.size - seq_length - 1, seq_length, 1});
-    tensor y = zeros({data.size - seq_length - 1, 1});
+    tensor x = zeros({data.size - seq_length, seq_length, 1});
+    tensor y = zeros({data.size - seq_length, 1});
 
     size_t idx = 0;
-    for (auto i = 0; i < (data.size - seq_length - 1) * seq_length; ++i) {
+    for (auto i = 0; i < (data.size - seq_length) * seq_length; ++i) {
         if (i % seq_length == 0 && i != 0)
             idx -= seq_length - 1;
         x[i] = data[idx];
         ++idx;
     }
 
-    for (auto i = 0; i < data.size - seq_length - 1; ++i)
+    for (auto i = 0; i < data.size - seq_length; ++i)
         y[i] = data[i + seq_length];
 
     return std::make_pair(x, y);
 }
 
-tensor hyperbolic_tangent(const tensor &z_t) {
+tensor relu(const tensor &z_t) {
     tensor h_t = z_t;
 
     for (auto i = 0; i < z_t.size; ++i)
-        h_t.elem[i] = std::tanhf(z_t.elem[i]);
+        h_t.elem[i] = std::fmax(0.0f, z_t.elem[i]);
 
     return h_t;
 }
 
 float mean_squared_error(const tensor &y_true, const tensor &y_pred) {
     float sum = 0.0f;
+    float n = static_cast<float>(y_true.shape.back());
 
     for (auto i = 0; i < y_true.size; ++i)
         sum += std::powf(y_true[i] - y_pred[i], 2.0f);
 
-    return sum / y_true.size;
+    return sum / n;
 }
 
 int main() {
@@ -47,8 +48,19 @@ int main() {
     auto x_y_train = create_sequences(train_test.first, 10);
     auto x_y_test = create_sequences(train_test.second, 10);
 
-    rnn model = rnn(hyperbolic_tangent, mean_squared_error, 0.01f);
+    // Everything much with google colab up to here! (delete this after everything is done)
+
+    rnn model = rnn(relu, mean_squared_error, 0.01f);
     model.train(x_y_train.first, x_y_train.second, x_y_test.first, x_y_test.second);
+
+    auto train_loss = model.evaluate(x_y_train.first, x_y_train.second);
+    auto test_loss = model.evaluate(x_y_test.first, x_y_test.second);
+
+    auto predict = model.predict(x_y_test.first);
+
+    std::cout << "Train loss: " << train_loss << std::endl;
+    std::cout << "Test  loss: " << test_loss << std::endl;
+    // std::cout << std::endl << predict << std::endl;
 
     return 0;
 }
