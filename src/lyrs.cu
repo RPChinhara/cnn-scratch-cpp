ebf4ab7a795c4f02c652801119f9ec3a490ae2d3
kimi <heipo_nogu@outlook.jp>
2024-10-15 19:48:34 +0900

    make it compact

--- src/lyrs.cu
+++ src/lyrs.cu
@@ -199,14 +199,7 @@ tensor da_dz(const tensor &a) {
     tensor t_new = a;
 
     for (auto i = 0; i < a.size; ++i)
-    {
-        if (0.0f < a[i])
-            t_new[i] = 1.0f;
-        else if (a[i] == 0.0f)
-            t_new[i] = 0.0f;
-        else
-            t_new[i] = 0.0f;
-    }
+        t_new[i] = (a[i] > 0.0f) ? 1.0f : 0.0f;
 
     return t_new;
 }
