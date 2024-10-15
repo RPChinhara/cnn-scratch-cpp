9b4dce001a94ec69757992c872e93231c4a6be09
kimi <heipo_nogu@outlook.jp>
2024-10-15 19:14:45 +0900

    house keeping

--- src/lyrs.cu
+++ src/lyrs.cu
@@ -443,8 +443,8 @@ void rnn::train(const tensor &x_train, const tensor &y_train, const tensor &x_va
                 d_loss_d_h_t = matmul(d_loss_d_h_t * transpose(1.0f - square(h_sequence[j + 1])), w_hh);
             }
 
-            d_loss_d_w_xh = d_loss_d_w_xh + matmul((transpose(d_loss_d_h_t) * derivative_relu(h_sequence[j])), x_sequence[j - 1]);
-            d_loss_d_w_hh = d_loss_d_w_hh + matmul((transpose(d_loss_d_h_t) * derivative_relu(h_sequence[j])), transpose(h_sequence[j - 1]));
+            d_loss_d_w_xh = d_loss_d_w_xh + matmul((transpose(d_loss_d_h_t) * relu_derivative(h_sequence[j])), x_sequence[j - 1]);
+            d_loss_d_w_hh = d_loss_d_w_hh + matmul((transpose(d_loss_d_h_t) * relu_derivative(h_sequence[j])), transpose(h_sequence[j - 1]));
 
             d_loss_d_b_h  = d_loss_d_b_h + transpose(d_loss_d_h_t);
         }
