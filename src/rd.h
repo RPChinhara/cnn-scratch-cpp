ad358d2697906ab10e6401a13605ce6b8a58e8b7
kimi <heipo_nogu@outlook.jp>
2024-10-17 22:52:20 +0900

    change config

--- src/rd.h
+++ src/rd.h
@@ -4,7 +4,7 @@
 
 class tensor;
 
-tensor glorot_uniform(const size_t fan_in, const size_t fan_out);
+tensor glorot_uniform(const size_t num_out, const size_t num_in);
 tensor normal_dist(const std::vector<size_t> &shape, const float mean = 0.0f, const float std_dev = 0.05f);
 tensor shuffle(const tensor &t, const size_t rd_state);
 tensor uniform_dist(const std::vector<size_t> &shape, const float min_val = -0.05f, const float max_val = 0.05f);
