f2ee04004eb311183cabb4ff058f6ecdb40e5382
KKK <heipo_nogu@outlook.jp>
2024-10-23 03:33:13 +0900

    upd lstm.cpp

--- examples/lstm.cpp
+++ examples/lstm.cpp
@@ -1,6 +1,65 @@
+#include "arrs.h"
 #include "datas.h"
 #include "lyrs.h"
+#include "preproc.h"
+
+std::pair<tensor, tensor> create_sequences(const tensor &data, const size_t seq_length) {
+    tensor x = zeros({data.size - seq_length, seq_length, 1});
+    tensor y = zeros({data.size - seq_length, 1});
+
+    size_t idx = 0;
+    for (auto i = 0; i < (data.size - seq_length) * seq_length; ++i) {
+        if (i % seq_length == 0 && i != 0)
+            idx -= seq_length - 1;
+        x[i] = data[idx];
+        ++idx;
+    }
+
+    for (auto i = 0; i < data.size - seq_length; ++i)
+        y[i] = data[i + seq_length];
+
+    return std::make_pair(x, y);
+}
+
+tensor sigmoid(const tensor &t) {
+    tensor t_new = t;
+
+    return 1.0 / (1.0 + exp(-t));
+}
+
+tensor hyperbolic_tangent(const tensor &z_t) {
+    tensor h_t = z_t;
+
+    for (auto i = 0; i < z_t.size; ++i)
+        h_t.elem[i] = std::tanhf(z_t.elem[i]);
+
+    return h_t;
+}
+
+float mean_squared_error(const tensor &y_true, const tensor &y_pred) {
+    float sum = 0.0f;
+    float n = static_cast<float>(y_true.shape.back());
+
+    for (auto i = 0; i < y_true.size; ++i)
+        sum += std::powf(y_true[i] - y_pred[i], 2.0f);
+
+    return sum / n;
+}
 
 int main() {
+    tensor data = load_aapl();
+
+    min_max_scaler2 scaler;
+    scaler.fit(data);
+    tensor scaled_data = scaler.transform(data);
+
+    auto train_test = split(scaled_data, 0.2f);
+
+    auto x_y_train = create_sequences(train_test.first, 10);
+    auto x_y_test = create_sequences(train_test.second, 10);
+
+    lstm model = lstm(hyperbolic_tangent, mean_squared_error, 0.01f);
+    model.train(x_y_train.first, x_y_train.second);
+
     return 0;
 }
