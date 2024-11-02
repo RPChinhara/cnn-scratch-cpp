5fd522078bd4227f5e92dfbc02f77e45dd311136
kimi <heipo_nogu@outlook.jp>
2024-11-02 17:30:40 +0900

    preparing the dataset

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,50 @@
 #include "lyrs.h"
 
-int main() {
+#include <fstream>
+#include <sstream>
 
+tensor load_daily_dialog() {
+    std::ifstream file("datas/daily_dialog.csv");
+
+    if (!file.is_open())
+        std::cerr << "Failed to open the file." << std::endl;
+
+    std::vector<std::string> data;
+
+    std::string line;
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string value;
+
+        std::getline(ss, value, ']');
+
+        value = regex_replace(value, R"((https?:\/\/|www\.)\S+)", "");
+        value = regex_replace(value, "<[^>]*>", " ");
+        value = regex_replace(value, "\"", "");
+        value = regex_replace(value, "[\".,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
+        value = regex_replace(value, "[^\\x00-\\x7f]", " ");
+        value = regex_replace(value, "[\xE2\x98\x80-\xE2\x9B\xBF]", "");
+        value = regex_replace(value, "\\s+", " ");
+
+        data.push_back(value);
+    }
+
+    std::cout << data[0] << std::endl;
+    std::cout << data[1] << std::endl;
+    std::cout << data[data.size() - 1] << std::endl;
+
+    file.close();
+
+    return tensor();
+}
+
+int main() {
+    tensor data = load_daily_dialog();
 
     gru model = gru(0.01f);
     // model.train(x_y_train.first, x_y_train.second);
-
-    std::cout << 1 << std::endl;
 
     return 0;
 }
