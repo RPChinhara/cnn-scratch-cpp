58427f87d6d6d6b7f1ab96a94c1e6cf458d4c26e
kimi <heipo_nogu@outlook.jp>
2024-11-03 18:24:47 +0900

    loading datast

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include <fstream>
 #include <sstream>
 
-tensor daily_dialog() {
-    std::ifstream file("datas/daily_dialog.csv");
+tensor daily_dialog_input() {
+    std::ifstream file("datas/daily_dialog/daily_dialog_input.csv");
 
     if (!file.is_open())
         std::cerr << "Failed to open the file." << std::endl;
 
-    std::vector<std::string> data;
+    std::vector<std::string> input;
 
     std::string line;
     std::getline(file, line);
@@ -18,7 +18,7 @@ tensor daily_dialog() {
         std::stringstream ss(line);
         std::string value;
 
-        std::getline(ss, value, ']');
+        std::getline(ss, value);
 
         value = lower(value);
         value = regex_replace(value, "[\".,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
@@ -27,11 +27,43 @@ tensor daily_dialog() {
         // value = regex_replace(value, "'", "");
         value = regex_replace(value, "\\s+", " ");
 
-        data.push_back(value);
+        input.push_back(value);
+    }
+
+    file.close();
+
+    return tensor();
+}
+
+tensor daily_dialog_target() {
+    std::ifstream file("datas/daily_dialog/daily_dialog_target.csv");
+
+    if (!file.is_open())
+        std::cerr << "Failed to open the file." << std::endl;
+
+    std::vector<std::string> target;
+
+    std::string line;
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string value;
+
+        std::getline(ss, value);
+
+        value = lower(value);
+        value = regex_replace(value, "[\".,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
+        value = regex_replace(value, "\\s*[^\\x00-\\x7f]\\s*", "");
+        value = regex_replace(value, "[^\\x00-\\x7f]", "");
+        // value = regex_replace(value, "'", "");
+        value = regex_replace(value, "\\s+", " ");
+
+        target.push_back(value);
     }
 
     for (auto i = 0; i < 10; ++i)
-        std::cout << data[i] << std::endl;
+        std::cout << target[i] << std::endl;
 
     file.close();
 
@@ -39,7 +71,8 @@ tensor daily_dialog() {
 }
 
 int main() {
-    tensor data = daily_dialog();
+    tensor x = daily_dialog_input();
+    tensor y = daily_dialog_target();
 
     gru model = gru(0.01f);
     // model.train(x_y_train.first, x_y_train.second);
