3cfa5bb74a810733cbbf926f33725cd488182f53
kimi <heipo_nogu@outlook.jp>
2024-11-03 18:59:21 +0900

    combine func

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include <fstream>
 #include <sstream>
 
-tensor daily_dialog_input() {
-    std::ifstream file("datas/daily_dialog/daily_dialog_input.csv");
+tensor daily_dialog(const std::string& file_path) {
+    std::ifstream file(file_path);
 
     if (!file.is_open())
-        std::cerr << "Failed to open the file." << std::endl;
+        std::cerr << "Failed to open the file: " << file_path << std::endl;
 
-    std::vector<std::string> input;
-
+    std::vector<std::string> data;
+    
     std::string line;
     std::getline(file, line);
 
@@ -27,52 +27,21 @@ tensor daily_dialog_input() {
         // value = regex_replace(value, "'", "");
         value = regex_replace(value, "\\s+", " ");
 
-        input.push_back(value);
+        data.push_back(value);
     }
 
     file.close();
 
-    return tensor();
-}
-
-tensor daily_dialog_target() {
-    std::ifstream file("datas/daily_dialog/daily_dialog_target.csv");
-
-    if (!file.is_open())
-        std::cerr << "Failed to open the file." << std::endl;
-
-    std::vector<std::string> target;
-
-    std::string line;
-    std::getline(file, line);
-
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        std::string value;
-
-        std::getline(ss, value);
-
-        value = lower(value);
-        value = regex_replace(value, "[\".,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
-        value = regex_replace(value, "\\s*[^\\x00-\\x7f]\\s*", "");
-        value = regex_replace(value, "[^\\x00-\\x7f]", "");
-        // value = regex_replace(value, "'", "");
-        value = regex_replace(value, "\\s+", " ");
-
-        target.push_back(value);
+    for (size_t i = 0; i < 10; ++i) {
+        std::cout << data[i] << std::endl;
     }
-
-    for (auto i = 0; i < 10; ++i)
-        std::cout << target[i] << std::endl;
-
-    file.close();
 
     return tensor();
 }
 
 int main() {
-    tensor x = daily_dialog_input();
-    tensor y = daily_dialog_target();
+    tensor input = daily_dialog("datas/daily_dialog/daily_dialog_input.csv");
+    tensor target = daily_dialog("datas/daily_dialog/daily_dialog_target.csv");
 
     gru model = gru(0.01f);
     // model.train(x_y_train.first, x_y_train.second);
