95c1b2362e2676f5530bc0eed28c41f40e1c331b
kimi <heipo_nogu@outlook.jp>
2024-11-04 17:42:40 +0900

    change the order

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,8 @@ tensor daily_dialog(const std::string& file_path) {
 
         std::getline(ss, value);
 
-        value = lower(value);
         value = regex_replace(value, "[.,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
+        value = lower(value);
         value = regex_replace(value, "\"", "");
         value = regex_replace(value, "\\s*[^\\x00-\\x7f]\\s*", "");
         value = regex_replace(value, "[^\\x00-\\x7f]", "");
@@ -47,6 +47,13 @@ int main() {
 
     gru model = gru(0.01f);
     // model.train(x_y_train.first, x_y_train.second);
+
+    std::vector<std::string> vocab = {"[START]", "say", "jim", "how", "about", "going", "for", "a", "few", "beers", "after", "dinner"};
+    std::vector<std::string> vocab2 = {"[START] say jim how about going for a few beers after dinner"};
+    std::vector<std::string> in = {"say say jim jim jim", "for a say cat how jim", "dog"};
+
+    std::cout << text_vectorization(vocab, in, 5000, 4) << std::endl;
+    std::cout << text_vectorization(vocab2, in, 5000, 4) << std::endl;
 
     return 0;
 }
