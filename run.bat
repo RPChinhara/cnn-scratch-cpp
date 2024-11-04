a3060de7c46a349a59c7584cd7c00545497c4bd7
kimi <heipo_nogu@outlook.jp>
2024-11-04 09:13:31 +0900

    change to cpp

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 set NVCC=nvcc
 set FLAGS=-arch sm_75 -I ..\src -o dora --optimize 3 -std c++20 --threads 0 --use_fast_math -Xcompiler /MP -Xcompiler /O2 -Xcompiler /utf-8
-set SRC=..\src\acts.cpp ..\src\arrs.cpp ..\src\datas.cpp ..\src\linalg.cu ..\src\losses.cpp ..\src\lyrs.cu ..\src\main.cpp ..\src\math.cu ..\src\preproc.cpp ..\src\rd.cpp ..\src\tensor.cpp
+set SRC=..\src\acts.cpp ..\src\arrs.cpp ..\src\datas.cpp ..\src\linalg.cu ..\src\losses.cpp ..\src\lyrs.cpp ..\src\main.cpp ..\src\math.cu ..\src\preproc.cpp ..\src\rd.cpp ..\src\tensor.cpp
 
 if not exist bin mkdir bin
 
