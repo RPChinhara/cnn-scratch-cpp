4ad902580b151e4bc64e6c35ac16aaa7b288dbb8
kimi <heipo_nogu@outlook.jp>
2024-11-04 19:12:08 +0900

    almost done preparing dataset

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <sstream>
 
-tensor daily_dialog(const std::string& file_path) {
+std::vector<std::string> daily_dialog(const std::string& file_path) {
     std::ifstream file(file_path);
 
     if (!file.is_open())
@@ -20,13 +20,14 @@ tensor daily_dialog(const std::string& file_path) {
 
         std::getline(ss, value);
 
-        value = regex_replace(value, "[.,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
         value = lower(value);
+        value = regex_replace(value, "[.,!?#$%&()*+/:;<=>@\\[\\]\\^_`{|}~\\\\-]", " ");
         value = regex_replace(value, "\"", "");
         value = regex_replace(value, "\\s*[^\\x00-\\x7f]\\s*", "");
         value = regex_replace(value, "[^\\x00-\\x7f]", "");
         // value = regex_replace(value, "'", "");
         value = regex_replace(value, "\\s+", " ");
+        value = regex_replace(value, "\\s+$", "");
         value = value.insert(0, "[START] ");
 
         data.push_back(value);
@@ -34,26 +35,38 @@ tensor daily_dialog(const std::string& file_path) {
 
     file.close();
 
-    for (size_t i = 0; i < 10; ++i) {
-        std::cout << data[i] << std::endl;
-    }
-
-    return tensor();
+    return data;
 }
 
 int main() {
-    tensor input = daily_dialog("datas/daily_dialog/daily_dialog_input.csv");
-    tensor target = daily_dialog("datas/daily_dialog/daily_dialog_target.csv");
+    auto input = daily_dialog("datas/daily_dialog/daily_dialog_input.csv");
+    auto target = daily_dialog("datas/daily_dialog/daily_dialog_target.csv");
+
+    std::vector<std::string> first_two_input;
+    std::vector<std::string> first_two_target;
+
+    for (size_t i = 0; i < 4; ++i)
+        first_two_input.push_back(input[i]);
+
+    for (size_t i = 0; i < 4; ++i)
+        first_two_target.push_back(target[i]);
+
+    for (size_t i = 0; i < 4; ++i)
+        std::cout << first_two_input[i] << std::endl;
+
+    std::cout << text_vectorization(first_two_input, first_two_input, 5000, 4) << std::endl;
+
+     for (size_t i = 0; i < 4; ++i)
+        std::cout << first_two_target[i] << std::endl;
+
+    // std::vector<std::string> in = {"say say jim jim jim", "for a say cat how jim", "dog"};
+
+    std::cout << text_vectorization(first_two_input, first_two_target, 5000, 4) << std::endl;
+
+    //TODO: What should max_len be?
 
     gru model = gru(0.01f);
     // model.train(x_y_train.first, x_y_train.second);
-
-    std::vector<std::string> vocab = {"[START]", "say", "jim", "how", "about", "going", "for", "a", "few", "beers", "after", "dinner"};
-    std::vector<std::string> vocab2 = {"[START] say jim how about going for a few beers after dinner"};
-    std::vector<std::string> in = {"say say jim jim jim", "for a say cat how jim", "dog"};
-
-    std::cout << text_vectorization(vocab, in, 5000, 4) << std::endl;
-    std::cout << text_vectorization(vocab2, in, 5000, 4) << std::endl;
 
     return 0;
 }
