4a479cf0e5bc95cc49fc115f01aaef081f7b179e
kimi <heipo_nogu@outlook.jp>
2024-11-12 23:07:29 +0900

    change name

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 #include <chrono>
 
-class cnn2d {
+class cnn_2d {
   private:
     std::vector<size_t> filters;
     float lr;
@@ -19,17 +19,17 @@ class cnn2d {
     std::vector<tensor> forward(const tensor &input, const std::vector<tensor> &kernel, const size_t stride);
 
   public:
-    cnn2d(const std::vector<size_t> &filters, float const lr);
+    cnn_2d(const std::vector<size_t> &filters, float const lr);
     void train(const tensor &x_train, const tensor &y_train, const tensor &x_val, const tensor &y_val);
     void predict(const tensor &xTest, const tensor &yTest);
 };
 
-cnn2d::cnn2d(const std::vector<size_t> &filters, float const lr) {
+cnn_2d::cnn_2d(const std::vector<size_t> &filters, float const lr) {
     this->filters = filters;
     this->lr = lr;
 }
 
-void cnn2d::train(const tensor &xTrain, const tensor &yTrain, const tensor &xVal, const tensor &yVal) {
+void cnn_2d::train(const tensor &xTrain, const tensor &yTrain, const tensor &xVal, const tensor &yVal) {
     tensor kernel = tensor({3, 3}, {1, -1, 1, 0, 1, 0, -1, 0, 1});
 
     size_t kernelHeight = kernel.shape.front();
@@ -44,10 +44,10 @@ void cnn2d::train(const tensor &xTrain, const tensor &yTrain, const tensor &xVal
     tensor output = zeros({outputHeight, outputWidth});
 }
 
-void cnn2d::predict(const tensor &xTest, const tensor &yTest) {
+void cnn_2d::predict(const tensor &xTest, const tensor &yTest) {
 }
 
-std::vector<tensor> cnn2d::forward(const tensor &input, const std::vector<tensor> &kernel, const size_t stride) {
+std::vector<tensor> cnn_2d::forward(const tensor &input, const std::vector<tensor> &kernel, const size_t stride) {
     std::vector<tensor> weights;
 
     return weights;
@@ -68,8 +68,8 @@ int main() {
     data.trainLabels = one_hot(data.trainLabels, 10);
     data.testLabels = one_hot(data.testLabels, 10);
 
-    cnn2d cnn2D = cnn2d({3, 128, 3}, 0.01f);
-    cnn2D.train(data.trainImages, data.trainLabels, data.testImages, data.testLabels);
+    cnn_2d model = cnn_2d({3, 128, 3}, 0.01f);
+    model.train(data.trainImages, data.trainLabels, data.testImages, data.testLabels);
 
     return 0;
 }
