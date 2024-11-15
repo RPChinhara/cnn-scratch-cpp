6b859e49ccdb83f728759c561d7f583762a41e73
kimi <heipo_nogu@outlook.jp>
2024-11-15 15:07:15 +0900

    same as before

--- src/arrs.h
+++ src/arrs.h
@@ -4,9 +4,9 @@
 
 class tensor;
 
-tensor clip_by_value(const tensor &t, float clip_val_min, float clip_val_max);
-tensor slice(const tensor &t, const size_t begin, const size_t size);
-tensor vslice(const tensor &t, const size_t col);
-std::pair<tensor, tensor> split(const tensor &t, const float test_size);
+tensor clip_by_value(const tensor& t, float clip_val_min, float clip_val_max);
+tensor slice(const tensor& t, const size_t begin, const size_t size);
+tensor vslice(const tensor& t, const size_t col);
+std::pair<tensor, tensor> split(const tensor& t, const float test_size);
 tensor vstack(const std::vector<tensor> &ts);
 tensor zeros(const std::vector<size_t> &shape);
