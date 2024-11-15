c8672d8d9a9f7a055b9c67271e6eeb5c6701d787
kimi <heipo_nogu@outlook.jp>
2024-11-15 16:40:56 +0900

    same as before

--- src/math.hpp
+++ src/math.hpp
@@ -2,11 +2,11 @@
 
 class tensor;
 
-tensor argmax(const tensor &t);
-tensor exp(const tensor &t);
-tensor log(const tensor &t);
-tensor max(const tensor &t, const size_t axis);
-tensor min(const tensor &t);
-tensor sqrt(const tensor &x);
-tensor square(const tensor &t);
-tensor sum(const tensor &t, const size_t axis);
+tensor argmax(const tensor& t);
+tensor exp(const tensor& t);
+tensor log(const tensor& t);
+tensor max(const tensor& t, const size_t axis);
+tensor min(const tensor& t);
+tensor sqrt(const tensor& x);
+tensor square(const tensor& t);
+tensor sum(const tensor& t, const size_t axis);
