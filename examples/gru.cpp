e00d78588abcccd788d41f71dc7b608f70879545
kimi <heipo_nogu@outlook.jp>
2024-11-18 22:09:30 +0900

    make it constexpr

--- examples/gru.cpp
+++ examples/gru.cpp
@@ -11,19 +11,19 @@
 #include <array>
 #include <chrono>
 
-float lr = 0.01f;
-size_t batch_size;
-size_t epochs = 150;
+constexpr float  lr          = 0.01f;
+size_t           batch_size  = 0;
+constexpr size_t epochs      = 150;
 
-size_t seq_length = 10;
-size_t input_size = 1;
-size_t hidden_size = 50;
-size_t output_size = 1;
+constexpr size_t seq_length  = 10;
+constexpr size_t input_size  = 1;
+constexpr size_t hidden_size = 50;
+constexpr size_t output_size = 1;
 
-float beta1 = 0.9f;
-float beta2 = 0.999f;
-float epsilon = 1e-7f;
-size_t t = 0;
+constexpr float  beta1       = 0.9f;
+constexpr float  beta2       = 0.999f;
+constexpr float  epsilon     = 1e-7f;
+size_t           t           = 0;
 
 tensor w_z = glorot_uniform(hidden_size, hidden_size + input_size);
 tensor w_r = glorot_uniform(hidden_size, hidden_size + input_size);
