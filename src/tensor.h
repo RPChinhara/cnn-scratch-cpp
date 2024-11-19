acc615acf5d69e13272ab0b72abcba6288733ff5
kimi <heipo_nogu@outlook.jp>
2024-11-19 22:57:25 +0900

    add reshape()

--- src/tensor.h
+++ src/tensor.h
@@ -18,6 +18,7 @@ class tensor {
     tensor& operator=(tensor&& other) noexcept;
 
     const std::string get_shape() const;
+    tensor& reshape(const std::vector<size_t>& new_shape);
 
     tensor operator+(const tensor& other) const;
     tensor operator-(const tensor& other) const;
