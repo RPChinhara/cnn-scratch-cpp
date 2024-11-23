935790ef6dd9b51eee0c0e7eac5656c86b540edf
kimi <heipo_nogu@outlook.jp>
2024-11-24 02:35:27 +0900

    change to x

--- src/acts.h
+++ src/acts.h
@@ -3,9 +3,9 @@
 class tensor;
 
 tensor hyperbolic_tangent(const tensor& x);
-tensor relu(const tensor& z);
-tensor sigmoid(const tensor& t);
-tensor softmax(const tensor& z);
+tensor relu(const tensor& x);
+tensor sigmoid(const tensor& x);
+tensor softmax(const tensor& x);
 
-tensor relu_derivative(const tensor& z);
-tensor sigmoid_derivative(const tensor& z);
+tensor relu_derivative(const tensor& x);
+tensor sigmoid_derivative(const tensor& x);
