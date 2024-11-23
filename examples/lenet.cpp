7642012909d43f62d6f4180ce4411551e03bd4a2
kimi <heipo_nogu@outlook.jp>
2024-11-24 00:45:25 +0900

    update

--- examples/lenet.cpp
+++ examples/lenet.cpp
@@ -10,28 +10,44 @@
 
 #include <chrono>
 
-constexpr float lr          = 0.01f;
-constexpr size_t batch_size = 32;
-constexpr size_t epochs     = 1;
+constexpr float  lr           = 0.01f;
+constexpr size_t batch_size   = 32;
+constexpr size_t epochs       = 10;
 
-tensor kernel1 = normal_dist({3, 3});
-tensor kernel2 = normal_dist({3, 3});
+constexpr size_t input_size   = 16;
+constexpr size_t hidden1_size = 120;
+constexpr size_t hidden2_size = 84;
+constexpr size_t output_size  = 10;
 
-tensor w1 = normal_dist({32 * 7 * 7});
-tensor b1 = zeros({1, 1});
+constexpr size_t num_kernels1 = 6;
+constexpr size_t num_kernels2 = 16;
 
-tensor w2 = normal_dist({128});
-tensor b2 = zeros({1, 1});
+tensor kernel1 = normal_dist({6, 5, 5});
+tensor kernel2 = normal_dist({6, 5, 5});
 
-tensor lenet_convolution(const tensor& x, const tensor& kernel, const size_t stride = 1, const size_t padding = 0) {
+tensor w1 = normal_dist({hidden1_size, input_size});
+tensor w2 = normal_dist({hidden2_size, hidden1_size});
+tensor w3 = normal_dist({output_size, hidden2_size});
+
+tensor b1 = zeros({hidden1_size, 1});
+tensor b2 = zeros({hidden2_size, 1});
+tensor b3 = zeros({output_size, 1});
+
+tensor lenet_convolution(const tensor& x, const tensor& kernels, const size_t stride = 1, const size_t padding = 0) {
+    if (kernels.shape.size() != 3) {
+        std::cerr << __FILE__ << "(" << __LINE__ << "): error: size of kernel should be 3" << std::endl;
+        exit(1);
+    }
+
     // Add padding to the input matrix here? For example,
     //        0 0 0 0
     // 1 1 -> 0 1 1 0
     // 1 1    0 1 1 0
     //        0 0 0 0
 
-    size_t kernel_height = kernel.shape.front();
-    size_t kernel_width = kernel.shape.back();
+    size_t num_kernels = kernels.shape.front();
+    size_t kernel_height = kernels.shape[1];
+    size_t kernel_width = kernels.shape.back();
 
     size_t input_height = x.shape[x.shape.size() - 2];
     size_t input_width = x.shape.back();
@@ -39,45 +55,57 @@ tensor lenet_convolution(const tensor& x, const tensor& kernel, const size_t str
     size_t output_height = (input_height - kernel_height) / stride + 1;
     size_t output_width = (input_width - kernel_width) / stride + 1;
 
-    tensor outputs = zeros({x.shape.front(), output_height, output_width});
+    tensor outputs = zeros({x.shape.front(), num_kernels, output_height, output_width});
 
+    size_t idx = 0;
     for (size_t b = 0; b < x.shape.front(); ++b) {
-        auto t = slice(x, b * input_height, input_height);
+        auto img = slice(x, b * input_height, input_height);
 
         tensor output = zeros({output_height, output_width});
 
-        for (size_t i = 0; i < output_height; ++i) {
-            for (size_t j = 0; j < output_width; ++j) {
-                float sum = 0.0;
+        for (size_t k = 0; k < num_kernels; ++k) {
+            auto kernel = slice(kernels, k * kernel_height, kernel_height);
 
-                for (size_t m = 0; m < kernel_height; ++m) {
-                    for (size_t n = 0; n < kernel_width; ++n) {
-                        sum += t(i + m, j + n) * kernel(m, n);
+            for (size_t i = 0; i < output_height; ++i) {
+                for (size_t j = 0; j < output_width; ++j) {
+                    float sum = 0.0;
+
+                    for (size_t m = 0; m < kernel_height; ++m) {
+                        for (size_t n = 0; n < kernel_width; ++n) {
+                            sum += img(i + m, j + n) * kernel(m, n);
+                        }
                     }
+
+                    output(i, j) = sum;
                 }
-
-                output(i, j) = sum;
             }
-        }
 
-        for (size_t i = 0; i < output.size; ++i)
-            outputs[b * output.size + i] = output[i];
+            for (size_t i = 0; i < output.size; ++i)
+                outputs[idx * output.size + i] = output[i];
+
+            ++idx;
+        }
     }
 
     return outputs;
 }
 
 tensor lenet_max_pool(const tensor& x, const size_t pool_size = 2, const size_t stride = 2) {
+    size_t num_kernels = x.shape[1];
+
     size_t input_height = x.shape[x.shape.size() - 2];
     size_t input_width = x.shape.back();
 
     size_t output_height = (input_height - pool_size) / stride + 1;
     size_t output_width = (input_width - pool_size) / stride + 1;
 
-    tensor outputs = zeros({x.shape.front(), output_height, output_width});
+    tensor outputs = zeros({x.shape.front(), num_kernels, output_height, output_width});
 
-    for (size_t b = 0; b < x.shape.front(); ++b) {
-        auto t = slice(x, b * input_height, input_height);
+    size_t batch_size = x.shape.front();
+    size_t num_img = batch_size * num_kernels;
+
+    for (size_t b = 0; b < num_img; ++b) {
+        auto img = slice(x, b * input_height, input_height);
 
         tensor output = zeros({output_height, output_width});
 
@@ -87,7 +115,7 @@ tensor lenet_max_pool(const tensor& x, const size_t pool_size = 2, const size_t 
 
                 for (size_t m = 0; m < pool_size; ++m) {
                     for (size_t n = 0; n < pool_size; ++n) {
-                        float val = t(i * stride + m, j * stride + n);
+                        float val = img(i * stride + m, j * stride + n);
 
                         if (val > max_val)
                             max_val = val;
@@ -106,19 +134,35 @@ tensor lenet_max_pool(const tensor& x, const size_t pool_size = 2, const size_t 
 }
 
 tensor lenet_forward(const tensor& x) {
-    auto x_conv1 = lenet_convolution(x, kernel1);
-    x_conv1 = relu(x_conv1);
-    x_conv1 = lenet_max_pool(x_conv1);
+    tensor c1 = lenet_convolution(x, kernel1);
+    c1 = relu(c1);
+    std::cout << c1.get_shape() << "\n";
 
-    auto x_conv2 = lenet_convolution(x_conv1, kernel2);
-    x_conv2 = relu(x_conv2);
-    x_conv2 = lenet_max_pool(x_conv2);
+    // tensor s2 = lenet_max_pool(c1);
+    // std::cout << s2.get_shape() << "\n";
 
-    // auto x_fc = matmul(w1, x_conv2) + b1;
-    // x_fc = matmul(w2, x_fc) + b2;
+    // tensor c3 = lenet_convolution(s2, kernel2);
+    // c3 = relu(c3);
+    // std::cout << c3.get_shape() << "\n";
 
-    // return x_fc;
+    // tensor s4 = lenet_max_pool(c3);
+    // std::cout << s4.get_shape() << "\n";
 
+    // TODO: Can I do x_conv2.reshape({25, 60000});?
+    // s4.reshape({60000, 16});
+
+    // (120, 16) * (16, 60000) -> (84, 120) * (120, 60000) -> (10, 84) * (84, 60000)
+
+    // tensor f5 = matmul(w1, transpose(s4)) + b1;
+    // std::cout << f5.get_shape() << "\n";
+
+    // tensor f6 = matmul(w2, f5) + b2;
+    // std::cout << f6.get_shape() << "\n";
+
+    // tensor y = softmax(matmul(w3, f6) + b3);
+    // std::cout << y.get_shape() << "\n";
+
+    // return y;
     return tensor();
 }
 
@@ -126,9 +170,29 @@ void lenet_train(const tensor& x_train, const tensor& y_train) {
     for (auto i = 1; i <= epochs; ++i) {
         auto start_time = std::chrono::high_resolution_clock::now();
 
-        auto y = lenet_forward(x_train);
+        tensor y = lenet_forward(x_train);
 
+        // float error = categorical_cross_entropy(y_train, transpose(y));
         float error = 0.0f;
+
+        // tensor d_loss_d_y = -2.0f / num_samples * (transpose(y_train) - y_sequence.front());
+
+        // for (auto j = seq_length; j > 0; --j) {
+        //     if (j == seq_length) {
+        //         tensor d_y_d_h_10 = w_hy;
+        //         d_loss_d_h_t = matmul(transpose(d_loss_d_y), d_y_d_h_10);
+        //     } else {
+        //         d_loss_d_h_t = matmul(d_loss_d_h_t * transpose(relu_derivative(z_sequence[j])), w_hh);
+        //     }
+
+        //     d_loss_d_w_xh = d_loss_d_w_xh + matmul((transpose(d_loss_d_h_t) * relu_derivative(z_sequence[j - 1])), x_sequence[j - 1]);
+        //     d_loss_d_w_hh = d_loss_d_w_hh + matmul((transpose(d_loss_d_h_t) * relu_derivative(z_sequence[j - 1])), transpose(h_sequence[j - 1]));
+
+        //     d_loss_d_b_h  = d_loss_d_b_h + sum(transpose(d_loss_d_h_t) * relu_derivative(z_sequence[j - 1]), 1);
+        // }
+
+        // w = w - lr * d_loss_d_w;
+        // b = b - lr * d_loss_d_y;
 
         auto end_time = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
@@ -140,42 +204,64 @@ void lenet_train(const tensor& x_train, const tensor& y_train) {
 }
 
 float lenet_evaluate(const tensor& x_test, const tensor& y_test) {
-    return 0.0f;
+    auto y = lenet_forward(x_test);
+    return categorical_cross_entropy(y_test, transpose(y));
 }
 
 void lenet_predict(const tensor& x_test, const tensor& y_test) {
 }
 
 int main() {
-    mnist data = load_mnist();
+    // mnist data = load_mnist();
 
-    constexpr size_t num_digits = 1;
-    constexpr size_t image_size = 784;
-    constexpr size_t image_dim = 28;
+    // constexpr size_t num_digits = 1;
+    // constexpr size_t img_size = 784;
+    // constexpr size_t img_dim = 28;
 
-    for (auto i = 0; i < num_digits; ++i) {
-        for (auto j = 0; j < image_size; ++j) {
-            if (j % image_dim == 0 && j != 0)
-                std::cout << std::endl;
+    // for (auto i = 0; i < num_digits; ++i) {
+    //     for (auto j = 0; j < img_size; ++j) {
+    //         if (j % img_dim == 0 && j != 0)
+    //             std::cout << std::endl;
 
-            std::cout << data.train_images[i * image_size + j] << " ";
-        }
+    //         std::cout << data.train_images[i * img_size + j] << " ";
+    //     }
 
-        std::cout << "\n\n";
-    }
+    //     std::cout << "\n\n";
+    // }
 
-    for (auto i = 0; i < data.train_images.size; ++i)
-        data.train_images[i] /= 255.0f;
+    // for (auto i = 0; i < data.train_images.size; ++i)
+    //     data.train_images[i] /= 255.0f;
 
-    for (auto i = 0; i < data.test_images.size; ++i)
-        data.test_images[i] /= 255.0f;
+    // for (auto i = 0; i < data.test_images.size; ++i)
+    //     data.test_images[i] /= 255.0f;
 
-    data.train_labels = one_hot(data.train_labels, 10);
-    data.test_labels = one_hot(data.test_labels, 10);
+    // // data.train_labels = one_hot(data.train_labels, 10);
+    // // data.test_labels = one_hot(data.test_labels, 10);
 
-    lenet_train(data.train_images, data.train_labels);
-    auto test_loss = lenet_evaluate(data.test_images, data.test_labels);
-    lenet_predict(data.test_images, data.test_labels);
+    // lenet_train(data.train_images, data.train_labels);
+    // // auto test_loss = lenet_evaluate(data.test_images, data.test_labels);
+    // // lenet_predict(data.test_images, data.test_labels);
+
+
+    tensor a = tensor({2, 2, 3, 3}, {44, 2, 22,
+                                     4, 8, 6,
+                                     5, 4, 66,
+
+                                     6, 5, 3,
+                                     4, 7, 8,
+                                     7, 32, 7,
+
+                                     1, 288, 8,
+                                     7, 4, 6,
+                                     5, 32, 6,
+
+                                     56, 1, 24,
+                                     4, 4, 6,
+                                     22, 5, 6,
+
+                                     });
+
+    std::cout << lenet_max_pool(a, 2, 1) << "\n";
 
     return 0;
 }
