885cc80f48e5c5d6d7eba00235e4bb175c5c68a8
kimi <heipo_nogu@outlook.jp>
2024-11-26 04:48:02 +0900

    rm log

--- src/math.hpp
+++ src/math.hpp
@@ -4,7 +4,6 @@ class tensor;
 
 tensor argmax(const tensor& t);
 tensor exp(const tensor& t);
-tensor log(const tensor& t);
 tensor max(const tensor& t, const size_t axis);
 tensor min(const tensor& t);
 tensor sqrt(const tensor& x);
