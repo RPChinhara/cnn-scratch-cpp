0f23ec1844f6180dd17cd95c3fe42104a11fc71b
kimi <heipo_nogu@outlook.jp>
2024-11-26 07:36:42 +0900

    house keeping

--- src/preproc.h
+++ src/preproc.h
@@ -5,13 +5,6 @@
 #include <string>
 #include <vector>
 
-struct train_test {
-    tensor x_train;
-    tensor y_train;
-    tensor x_test;
-    tensor y_test;
-};
-
 std::string lower(const std::string& text);
 tensor one_hot(const tensor& t, const size_t depth);
 std::string regex_replace(const std::string& in, const std::string& pattern, const std::string& rewrite);
