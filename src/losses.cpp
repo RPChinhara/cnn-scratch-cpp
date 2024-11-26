c52aa236e29eb42178dc4254950cc1f3f0123f9f
kimi <heipo_nogu@outlook.jp>
2024-11-26 23:42:02 +0900

    house keeping

--- src/losses.cpp
+++ src/losses.cpp
@@ -4,22 +4,21 @@
 
 float categorical_cross_entropy(const tensor& y_true, const tensor& y_pred) {
     float sum = 0.0f;
-    constexpr float epsilon = 1e-12f;
-    size_t num_samples = y_true.size;
-    tensor y_pred_clipped = clip_by_value(y_pred, epsilon, 1.0f);
+    float num_elm = static_cast<float>(y_true.size);
+    tensor y_pred_clipped = clip_by_value(y_pred, 1e-12f, 1.0f);
 
     for (auto i = 0; i < y_true.size; ++i)
         sum += y_true[i] * logf(y_pred_clipped[i]);
 
-    return -sum / num_samples;
+    return -sum / num_elm;
 }
 
 float mean_squared_error(const tensor& y_true, const tensor& y_pred) {
     float sum = 0.0f;
-    float n = static_cast<float>(y_true.size);
+    float num_elm = static_cast<float>(y_true.size);
 
     for (auto i = 0; i < y_true.size; ++i)
         sum += std::powf(y_true[i] - y_pred[i], 2.0f);
 
-    return sum / n;
+    return sum / num_elm;
 }
