6d0d5b1a563740f11775f60f9fb54f8b8176c558
kimi <heipo_nogu@outlook.jp>
2024-11-28 03:21:51 +0900

    rm is_fitted

--- src/lyrs.h
+++ src/lyrs.h
@@ -15,28 +15,20 @@ class min_max_scaler {
   private:
     tensor data_min;
     tensor data_max;
-    bool is_fitted;
 
   public:
-    min_max_scaler() : data_min(), data_max(), is_fitted(false) {}
+    min_max_scaler() = default;
 
     void fit(const tensor& data) {
         data_min = min(data);
         data_max = max(data, 0);
-        is_fitted = true;
     }
 
     tensor transform(const tensor& data) {
-        if (!is_fitted)
-            std::cerr << "Scaler not fitted yet." << std::endl;
-
         return (data - data_min) / (data_max - data_min);
     }
 
     tensor inverse_transform(const tensor& scaled_data) {
-       if (!is_fitted)
-            std::cerr << "Scaler not fitted yet." << std::endl;
-
         return scaled_data * (data_max - data_min) + data_min;
     }
 
