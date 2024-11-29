9d133d12eb65350375b113ade6481eaeac94b541
kimi <heipo_nogu@outlook.jp>
2024-11-30 03:47:35 +0900

    house keeping

--- src/main.cpp
+++ src/main.cpp
@@ -286,14 +286,6 @@ int main() {
 
     // std::cout << lenet_convolution(x1, kernel1) << "\n";
 
-    tensor x = zeros({3, 2, 2});
-    for (size_t i = 0; i < x.size; ++i) {
-        x[i] += 1.0f;
-    }
-
-    std::cout << x << "\n";
-    std::cout << pad(x, 1, 1, 1, 1) << "\n";
-
     // NOTE: oprator+ code
 
     // tensor x6 = zeros({1, 2});
