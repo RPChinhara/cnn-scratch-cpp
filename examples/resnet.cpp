74ff93755d21f53b374a793f578d704c8b93a2d2
kimi <heipo_nogu@outlook.jp>
2024-12-01 09:30:31 +0900

    add cnns

---
+++ examples/resnet.cpp
@@ -0,0 +1,17 @@
+#include "acts.h"
+#include "arrs.h"
+#include "datas.h"
+#include "linalg.h"
+#include "losses.h"
+#include "lyrs.h"
+#include "math.h"
+#include "rand.h"
+#include "tensor.h"
+
+#include <chrono>
+
+int main() {
+
+
+    return 0;
+}
