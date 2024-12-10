d7efebc8605d04647a7a3916e084a11ef70b7b4d
mon <yukawa_hidenori@icloud.com>
2024-12-10 19:26:58 +0900

    house keeping

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set NVCC=nvcc
-set FLAGS=-arch sm_75 -o dora --optimize 3 -std c++20 --threads 0 --use_fast_math -Xcompiler "/O2 /MT /MP"
+set FLAGS=-arch sm_75 -o dora -O3 -std c++20 --threads 0 --use_fast_math -Xcompiler "/O2 /MT /MP"
 set SRC=..\src\acts.cpp ..\src\arrs.cpp ..\src\datas.cpp ..\src\linalg.cu ..\src\losses.cpp ..\src\lyrs.cpp ..\src\main.cpp ..\src\math.cu ..\src\rand.cpp ..\src\strings.cpp ..\src\tensor.cpp
 
 if not exist bin mkdir bin
