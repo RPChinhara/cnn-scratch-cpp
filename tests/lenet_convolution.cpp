f79dc55cbf13fd259ac7b80219bd67a94c52a0a2
mon <yukawa_hidenori@icloud.com>
2024-12-13 18:32:16 +0900

    house keeping

--- tests/lenet_convolution.cpp
+++ tests/lenet_convolution.cpp
@@ -25,12 +25,12 @@ tensor lenet_convolution(const tensor& x, const tensor& kernels, const size_t st
         num_img = x.shape.front();
 
         for (size_t b = 0; b < num_img; ++b) {
-            auto img = slice(x, b * input_height, input_height);
+            tensor img = slice(x, b * input_height, input_height);
 
             tensor output = zeros({output_height, output_width});
 
             for (size_t k = 0; k < num_kernels; ++k) {
-                auto kernel = slice(kernels, k * kernel_height, kernel_height);
+                tensor kernel = slice(kernels, k * kernel_height, kernel_height);
 
                 for (size_t i = 0; i < output_height; ++i) {
                     for (size_t j = 0; j < output_width; ++j) {
