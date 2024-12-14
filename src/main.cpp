fe55521302b8379b389b9d6d6604df9377aba420
mon <yukawa_hidenori@icloud.com>
2024-12-14 19:37:27 +0900

    optimize conv()

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ tensor lenet_convolution(const tensor& x, const tensor& kernels, const size_t st
     size_t output_height = (input_height - kernel_height) / stride + 1;
     size_t output_width = (input_width - kernel_width) / stride + 1;
 
-    tensor outputs = zeros({x.shape.front(), num_kernels, output_height, output_width});
+    tensor feature_maps = zeros({x.shape.front(), num_kernels, output_height, output_width});
 
     size_t idx = 0;
 
@@ -84,7 +84,7 @@ tensor lenet_convolution(const tensor& x, const tensor& kernels, const size_t st
                 }
 
                 for (size_t i = 0; i < output.size; ++i)
-                    outputs[idx * output.size + i] = output[i];
+                    feature_maps[idx * output.size + i] = output[i];
 
                 ++idx;
             }
@@ -93,42 +93,50 @@ tensor lenet_convolution(const tensor& x, const tensor& kernels, const size_t st
         size_t num_batches = x.shape.front();
         size_t num_channels = x.shape[1];
 
+        // TODO: Can I only use for loops twice like above when the x.shape.size() == 3?
         for (size_t i = 0; i < num_batches; ++i) {
             for (size_t j = 0; j < num_kernels; ++j) {
                 tensor kernel = slice(kernels, j * kernel_height, kernel_height);
-                tensor output_sum = zeros({output_height, output_width});
+                tensor channels_sum = zeros({output_height, output_width});
 
                 for (size_t k = 0; k < num_channels; ++k) {
                     size_t idx = i * num_channels + k;
                     tensor img = slice(x, idx * input_height, input_height);
-                    tensor output = zeros({output_height, output_width});
 
-                    for (size_t i = 0; i < output_height; ++i) {
-                        for (size_t j = 0; j < output_width; ++j) {
+                    // TODO: This could be optimized.
+                    // If the size of kernel was 2 x 2, and img was 3 x 3.
+                    // kernel, img
+                    //         0 1 2
+                    // 1 1     3 4 5
+                    // 1 1     6 7 8
+                    // (0) * (1), (1) * (1), (3) * (1), (4) * (1) -> this is dot product between top left corners.
+                    // (1) * (1), (2) * (1), (4) * (1), (5) * (1) -> this is dot product between top right corners.
+                    // This way I don't need to use 4 for loops like now.
+
+                    for (size_t row = 0; row < output_height; ++row) {
+                        for (size_t col = 0; col < output_width; ++col) {
                             float sum = 0.0;
 
                             for (size_t m = 0; m < kernel_height; ++m) {
                                 for (size_t n = 0; n < kernel_width; ++n) {
-                                    sum += img(i + m, j + n) * kernel(m, n);
+                                    sum += img(row + m, col + n) * kernel(m, n);
                                 }
                             }
 
-                            output(i, j) = sum;
+                            channels_sum(row, col) += sum;
                         }
                     }
-
-                    output_sum += output;
                 }
 
-                for (size_t i = 0; i < output_sum.size; ++i)
-                    outputs[idx * output_sum.size + i] = output_sum[i];
+                for (size_t i = 0; i < channels_sum.size; ++i)
+                    feature_maps[idx * channels_sum.size + i] = channels_sum[i];
 
                 ++idx;
             }
         }
     }
 
-    return outputs;
+    return feature_maps;
 }
 
 tensor lenet_max_pool(const tensor& x, const size_t pool_size = 2, const size_t stride = 2) {
