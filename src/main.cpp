3c0a4f50752c84653c7af8d87d2b2722ee72f57d
mon <yukawa_hidenori@icloud.com>
2024-12-19 21:51:29 +0900

    add todo

--- src/main.cpp
+++ src/main.cpp
@@ -377,14 +377,14 @@ int main() {
 
     // lenet_predict(data.test_imgs, data.test_labels);
 
-    auto dl_ds3 = uniform_dist({2, 2, 3, 3}, 0.0f, 0.000001f);
+    auto dl_ds4 = uniform_dist({2, 2, 3, 3}, 0.0f, 0.000001f);
     auto dl_dc3 = zeros({2, 2, 6, 6});
     auto c3 = uniform_dist({2, 2, 6, 6}, 0.0f, 0.000001f);
     auto s4 = lenet_max_pool(c3);
 
     std::cout << c3 << "\n";
     std::cout << s4 << "\n";
-    std::cout << dl_ds3 << "\n";
+    std::cout << dl_ds4 << "\n";
 
     size_t idx = 0;
     size_t cumulative_height = 0;
@@ -398,6 +398,8 @@ int main() {
         for (size_t j = 0; j < output_img_size; ++j) {
             // TODO: Use eigther of these below
             // img(max_indices[idx].first, max_indices[idx].second) = 1.0f;
+
+            // TODO: Write to notes.txt that I omitted to assign 1.0f, and directly assigned dl_ds4
             dl_dc3(cumulative_height + max_indices[idx].first, max_indices[idx].second) = 1.0f;
 
             ++idx;
@@ -414,7 +416,7 @@ int main() {
     idx = 0;
     for (size_t i = 0; i < dl_dc3.size; ++i) {
         if (dl_dc3[i] == 1.0f) {
-            dl_dc3[i] = dl_ds3[idx];
+            dl_dc3[i] = dl_ds4[idx];
             ++idx;
         }
     }
