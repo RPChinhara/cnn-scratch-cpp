1310b26ca3ebae2078c34e5c0942080fc40a0409
mon <yukawa_hidenori@icloud.com>
2024-12-19 15:26:36 +0900

    change names

--- src/main.cpp
+++ src/main.cpp
@@ -345,25 +345,26 @@ int main() {
 
     // lenet_predict(data.test_imgs, data.test_labels);
 
-    auto x1 = uniform_dist({2, 2, 4, 4}, 0.0f, 0.000001f);
-    auto x2 = zeros({2, 2, 4, 4});
+    auto dl_c3 = zeros({2, 2, 6, 6});
+    auto c3 = uniform_dist({2, 2, 6, 6}, 0.0f, 0.000001f);
+    auto s4 = lenet_max_pool(c3);
 
-    std::cout << x1 << "\n";
-    std::cout << lenet_max_pool(x1) << "\n";
+    std::cout << c3 << "\n";
+    std::cout << s4 << "\n";
 
     size_t idx = 0;
     size_t cumulative_height = 0;
-    size_t num_imgs = x2.shape.front() * x2.shape[1]; // or lenet_max_pool(x1).shape.front() * lenet_max_pool(x1).shape[1];
-    size_t output_img_size = lenet_max_pool(x1).shape[2] * lenet_max_pool(x1).shape.back();
+    size_t num_imgs = s4.shape.front() * s4.shape[1];
+    size_t output_img_size = s4.shape[2] * s4.shape.back();
 
     for (size_t i = 0; i < num_imgs; ++i) {
-        size_t img_height = x2.shape[2];
+        size_t img_height = c3.shape[2];
         // auto img = slice(x2, i * img_height, img_height);
 
         for (size_t j = 0; j < output_img_size; ++j) {
             // TODO: Use eigther of these below
             // img(max_indices[idx].first, max_indices[idx].second) = 1.0f;
-            x2(cumulative_height + max_indices[idx].first, max_indices[idx].second) = 1.0f;
+            dl_c3(cumulative_height + max_indices[idx].first, max_indices[idx].second) = 1.0f;
 
             ++idx;
         }
@@ -371,7 +372,7 @@ int main() {
         cumulative_height += img_height;
     }
 
-    std::cout << x2 << "\n";
+    std::cout << dl_c3 << "\n";
 
     return 0;
 }
