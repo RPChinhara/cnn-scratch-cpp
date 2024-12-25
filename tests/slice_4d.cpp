0d9d595719ceec543732a62aa136d08f68bd2381
mon <yukawa_hidenori@icloud.com>
2024-12-26 04:55:08 +0900

    add test for slice

---
+++ tests/slice_4d.cpp
@@ -0,0 +1,11 @@
+#include "arrs.h"
+
+int main () {
+    tensor dl_dc3 = uniform_dist({3, 2, 4, 4}, 0.0f, 0.000001f);
+
+    std::cout << slice_4d(dl_dc3, 0) << "\n";
+    std::cout << slice_4d(dl_dc3, 1 * dl_dc3.shape[2] * dl_dc3.shape.back()) << "\n";
+    std::cout << slice_4d(dl_dc3, 2 * dl_dc3.shape[2] * dl_dc3.shape.back()) << "\n";
+
+    return 0;
+}
