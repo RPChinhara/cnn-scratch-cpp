84fa47053c45c9b0a433c58cab667390de515554
mon <yukawa_hidenori@icloud.com>
2024-12-26 19:09:08 +0900

    house keeping

--- tests/slice_4d.cpp
+++ tests/slice_4d.cpp
@@ -3,6 +3,7 @@
 int main () {
     tensor dl_dc3 = uniform_dist({3, 2, 4, 4}, 0.0f, 0.000001f);
 
+    std::cout << dl_dc3 << "\n";
     std::cout << slice_4d(dl_dc3, 0) << "\n";
     std::cout << slice_4d(dl_dc3, 1 * dl_dc3.shape[1] * dl_dc3.shape[2] * dl_dc3.shape.back()) << "\n";
     std::cout << slice_4d(dl_dc3, 2 * dl_dc3.shape[1] * dl_dc3.shape[2] * dl_dc3.shape.back()) << "\n";
