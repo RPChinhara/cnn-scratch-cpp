c2488f04ad9339dacd747ec82312adf0f2c0bd5c
mon <yukawa_hidenori@icloud.com>
2025-01-01 00:20:52 +0900

    add more batches

--- tests/slice_3d.cpp
+++ tests/slice_3d.cpp
@@ -4,17 +4,18 @@
 
 int main() {
 
-    auto a = uniform_dist({8, 2, 2}, 0.0f, 0.0000001f);
+    auto a = uniform_dist({12, 2, 2}, 0.0f, 0.0000001f);
 
     std::cout << slice_3d(a, 0, 1) << "\n";
     std::cout << slice_3d(a, 0, 2) << "\n";
     std::cout << slice_3d(a, 0, 3) << "\n";
-    
+
     std::cout << slice_3d(a, 1, 2) << "\n";
     std::cout << slice_3d(a, 2, 3) << "\n";
 
     std::cout << slice_3d(a, 0, 4) << "\n";
     std::cout << slice_3d(a, 4, 4) << "\n";
+    std::cout << slice_3d(a, 8, 4) << "\n";
 
     std::cout << a << std::endl;
 
