0d5018ed2021c899ffa7a67f80e9ff8a07a4e0d8
mon <yukawa_hidenori@icloud.com>
2025-01-05 09:04:02 +0900

    no support for 3d

--- tests/lenet_convolution.cpp
+++ tests/lenet_convolution.cpp
@@ -18,81 +18,48 @@ tensor convolution(const tensor& x, const tensor& kernels, const size_t stride =
     tensor feature_maps = zeros({x.shape.front(), num_kernels, output_height, output_width});
 
     size_t idx = 0;
+    size_t num_batches = x.shape.front();
+    size_t num_channels = x.shape[1];
 
-    if (x.shape.size() == 3) {
-        size_t num_img = x.shape.front();
+    // TODO: Can I only use for loops twice similar to when I had 'if (x.shape.size() == 3)'?
+    for (size_t i = 0; i < num_batches; ++i) {
+        for (size_t j = 0; j < num_kernels; ++j) {
+            tensor kernel = slice(kernels, j * kernel_height, kernel_height);
+            tensor channels_sum = zeros({output_height, output_width});
 
-        for (size_t b = 0; b < num_img; ++b) {
-            tensor img = slice(x, b * input_height, input_height);
-            tensor output = zeros({output_height, output_width});
+            for (size_t k = 0; k < num_channels; ++k) {
+                size_t idx = i * num_channels + k;
+                tensor img = slice(x, idx * input_height, input_height);
 
-            for (size_t k = 0; k < num_kernels; ++k) {
-                tensor kernel = slice(kernels, k * kernel_height, kernel_height);
+                // TODO: This could be optimized.
+                // If the size of kernel was 2 x 2, and img was 3 x 3.
+                // kernel, img
+                //         0 1 2
+                // 1 1     3 4 5
+                // 1 1     6 7 8
+                // (0) * (1), (1) * (1), (3) * (1), (4) * (1) -> this is dot product between top left corners.
+                // (1) * (1), (2) * (1), (4) * (1), (5) * (1) -> this is dot product between top right corners.
+                // This way I don't need to use 4 for loops like now.
 
-                for (size_t i = 0; i < output_height; ++i) {
-                    for (size_t j = 0; j < output_width; ++j) {
+                for (size_t row = 0; row < output_height; ++row) {
+                    for (size_t col = 0; col < output_width; ++col) {
                         float sum = 0.0;
 
                         for (size_t m = 0; m < kernel_height; ++m) {
                             for (size_t n = 0; n < kernel_width; ++n) {
-                                sum += img(i + m, j + n) * kernel(m, n);
+                                sum += img(row + m, col + n) * kernel(m, n);
                             }
                         }
 
-                        output(i, j) = sum;
+                        channels_sum(row, col) += sum;
                     }
                 }
-
-                for (size_t i = 0; i < output.size; ++i)
-                    feature_maps[idx * output.size + i] = output[i];
-
-                ++idx;
             }
-        }
-    } else if (x.shape.size() == 4) {
-        size_t num_batches = x.shape.front();
-        size_t num_channels = x.shape[1];
 
-        // TODO: Can I only use for loops twice like above when the x.shape.size() == 3?
-        for (size_t i = 0; i < num_batches; ++i) {
-            for (size_t j = 0; j < num_kernels; ++j) {
-                tensor kernel = slice(kernels, j * kernel_height, kernel_height);
-                tensor channels_sum = zeros({output_height, output_width});
+            for (size_t i = 0; i < channels_sum.size; ++i)
+                feature_maps[idx * channels_sum.size + i] = channels_sum[i];
 
-                for (size_t k = 0; k < num_channels; ++k) {
-                    size_t idx = i * num_channels + k;
-                    tensor img = slice(x, idx * input_height, input_height);
-
-                    // TODO: This could be optimized.
-                    // If the size of kernel was 2 x 2, and img was 3 x 3.
-                    // kernel, img
-                    //         0 1 2
-                    // 1 1     3 4 5
-                    // 1 1     6 7 8
-                    // (0) * (1), (1) * (1), (3) * (1), (4) * (1) -> this is dot product between top left corners.
-                    // (1) * (1), (2) * (1), (4) * (1), (5) * (1) -> this is dot product between top right corners.
-                    // This way I don't need to use 4 for loops like now.
-
-                    for (size_t row = 0; row < output_height; ++row) {
-                        for (size_t col = 0; col < output_width; ++col) {
-                            float sum = 0.0;
-
-                            for (size_t m = 0; m < kernel_height; ++m) {
-                                for (size_t n = 0; n < kernel_width; ++n) {
-                                    sum += img(row + m, col + n) * kernel(m, n);
-                                }
-                            }
-
-                            channels_sum(row, col) += sum;
-                        }
-                    }
-                }
-
-                for (size_t i = 0; i < channels_sum.size; ++i)
-                    feature_maps[idx * channels_sum.size + i] = channels_sum[i];
-
-                ++idx;
-            }
+            ++idx;
         }
     }
 
