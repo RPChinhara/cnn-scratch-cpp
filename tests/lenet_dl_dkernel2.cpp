7db17c0369dcc461477cc80a68e0541151a6f248
mon <yukawa_hidenori@icloud.com>
2025-01-08 10:51:48 +0900

    almost done

--- tests/lenet_dl_dkernel2.cpp
+++ tests/lenet_dl_dkernel2.cpp
@@ -72,28 +72,62 @@ tensor convolution(const tensor& x, const tensor& kernels, const size_t stride =
 }
 
 int main () {
-    tensor s2 = uniform_dist({1, 6, 14, 14}, 0.0f, 0.0001f); // real shape = (64, 6, 14, 14)
-    tensor dl_dc3 = uniform_dist({1, 16, 10, 10}, 0.0f, 0.0001f); // real shape = (64, 16, 10, 10)
+    size_t batch_size = 10;
+
+    tensor s2 = uniform_dist({batch_size, 6, 14, 14}, 0.0f, 0.0001f); // real shape = (64, 6, 14, 14)
+    tensor dl_dc3 = uniform_dist({batch_size, 16, 10, 10}, 0.0f, 0.0001f); // real shape = (64, 16, 10, 10)
     tensor dl_dkernel2 = zeros({16, 6, 5, 5});
 
-    for (size_t i = 0; i < 1; ++i) {
-        auto img_4d = slice_4d(s2, i, 1);
-        auto kernel = slice_4d(dl_dc3, i, 1);
+    // NOTE: Smaller shapes for test
+    // tensor s2 = zeros({batch_size, 3, 4, 4});
+    // tensor dl_dc3 = zeros({batch_size, 2, 2, 2});
+    // tensor dl_dkernel2 = zeros({2, 3, 3, 3});
 
-        std::cout << img_4d << std::endl;
-        // std::cout << kernel << std::endl;
+    // for (size_t i = 0; i < s2.size; ++i)
+    //     s2[i] = i;
+
+    // for (size_t i = 0; i < dl_dc3.size; ++i)
+    //     dl_dc3[i] = i;
+
+    size_t input_channels = s2.shape[1];
+    size_t output_channels = dl_dc3.shape[1];
+
+    for (size_t i = 0; i < batch_size; ++i) {
+        tensor s2_4d = slice_4d(s2, i, 1);
+        tensor dl_dc3_4d = slice_4d(dl_dc3, i, 1);
+
+        tensor dl_dkernel2_batch = zeros({16, 6, 5, 5});
+        size_t idx = 0;
 
         for (size_t j = 0; j < 16; ++j) {
-            auto kernel_2d = slice(kernel_4d, j * 10, 10);
+            tensor dl_dc3_2d = slice(dl_dc3_4d, j * 10, 10);
+            dl_dc3_2d.reshape({1, 1, 10, 10});
+
+            // NOTE: Smaller shapes for test
+            // tensor dl_dc3_2d = slice(dl_dc3_4d, j * 2, 2);
+            // dl_dc3_2d.reshape({1, 1, 2, 2});
 
             for (size_t k = 0; k < 6; ++k) {
-                auto img_2d = slice(img_4d, k * 14, 14);
-                std::cout << img_2d << std::endl;
+                tensor s2_2d = slice(s2_4d, k * 14, 14);
+                s2_2d.reshape({1, 1, 14, 14});
 
-                // dl_dkernel2 += convolution(img_2d, kernel_2d);
+                // NOTE: Smaller shapes for test
+                // tensor s2_2d = slice(s2_4d, k * 4, 4);
+                // s2_2d.reshape({1, 1, 4, 4});
+
+                tensor feature_map = convolution(s2_2d, dl_dc3_2d); // TODO: These are not 2d anymore so change names
+
+                for (size_t l = 0; l < feature_map.size; ++l)
+                    dl_dkernel2_batch[idx * feature_map.size + l] = feature_map[l];
+
+                ++idx;
             }
         }
+
+        dl_dkernel2 += dl_dkernel2_batch;
     }
+
+    std::cout << dl_dkernel2 << std::endl;
 
     return 0;
 }
