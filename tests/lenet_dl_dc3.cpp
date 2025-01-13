b745164c491438bdda7b8d66f3fed52f2a83f800
kimi <heipo_nogu@outlook.jp>
2025-01-13 19:09:35 +0900

    house keeping

--- tests/lenet_dl_dc3.cpp
+++ tests/lenet_dl_dc3.cpp
@@ -4,8 +4,7 @@
 
 std::vector<std::pair<size_t, size_t>> max_indices;
 
-// TODO: Move this to the lyrs folders?
-tensor lenet_max_pool(const tensor& x, const size_t pool_size = 2, const size_t stride = 2) {
+tensor max_pool(const tensor& x, const size_t pool_size = 2, const size_t stride = 2) {
     size_t num_kernels = x.shape[1];
 
     size_t input_height = x.shape[x.shape.size() - 2];
@@ -57,27 +56,20 @@ int main () {
     auto dl_ds4 = uniform_dist({2, 2, 3, 3}, 0.0f, 0.000001f);
     auto dl_dc3 = zeros({2, 2, 6, 6});
     auto c3 = uniform_dist({2, 2, 6, 6}, 0.0f, 0.000001f);
-    auto s4 = lenet_max_pool(c3);
+    auto s4 = max_pool(c3);
 
     std::cout << c3 << "\n";
     std::cout << s4 << "\n";
     std::cout << dl_ds4 << "\n";
 
-    size_t idx = 0;
-    size_t cumulative_height = 0;
     size_t num_imgs = c3.shape.front() * c3.shape[1];
     size_t output_img_size = dl_ds4.shape[2] * dl_ds4.shape.back();
+    size_t cumulative_height = 0;
+    size_t idx = 0;
+    size_t img_height = c3.shape[2];
 
     for (size_t i = 0; i < num_imgs; ++i) {
-        size_t img_height = c3.shape[2];
-        // auto img = slice(x2, i * img_height, img_height);
-
         for (size_t j = 0; j < output_img_size; ++j) {
-            // TODO: Use eigther of these below
-            // img(max_indices[idx].first, max_indices[idx].second) = 1.0f;
-
-            // TODO: Write notes.txt that I omitted to assign 1.0f, and directly assigned dl_ds4
-            // dl_dc3(cumulative_height + max_indices[idx].first, max_indices[idx].second) = 1.0f;
             dl_dc3(cumulative_height + max_indices[idx].first, max_indices[idx].second) = dl_ds4[idx];
 
             ++idx;
