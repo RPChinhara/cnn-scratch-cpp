e94911347d41a16995d648f8f33b3550e2af08d6
kimi <heipo_nogu@outlook.jp>
2025-01-16 19:41:26 +0900

    fix fucking loss

--- src/losses.cpp
+++ src/losses.cpp
@@ -4,7 +4,7 @@
 
 float categorical_cross_entropy(const tensor& y_true, const tensor& y_pred) {
     float sum = 0.0f;
-    float num_elm = static_cast<float>(y_true.size);
+    float num_elm = static_cast<float>(y_true.shape.front());
     tensor y_pred_clipped = clip_by_value(y_pred, 1e-12f, 1.0f);
 
     for (auto i = 0; i < y_true.size; ++i)
@@ -15,7 +15,7 @@ float categorical_cross_entropy(const tensor& y_true, const tensor& y_pred) {
 
 float mean_squared_error(const tensor& y_true, const tensor& y_pred) {
     float sum = 0.0f;
-    float num_elm = static_cast<float>(y_true.size);
+    float num_elm = static_cast<float>(y_true.size); // FIX: This is wrong too?
 
     for (auto i = 0; i < y_true.size; ++i)
         sum += std::powf(y_true[i] - y_pred[i], 2.0f);
