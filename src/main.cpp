64eb0a4bfe4590d1d9ae9b1f97ac01e33f56636f
Sora <yukawa_hidenori@icloud.com>
2025-01-22 21:32:02 +0900

    add a todo

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@ int main() {
     auto target = load_daily_dialog("datasets/daily_dialog/daily_dialog_target.csv");
 
     // TODO: If I make text_vectorization() a class, runtime will be 1/2 of now as I only need to create the vocabulary once for "input_target". I don't need to do it twice.
+    // TODO: I may need to use subword tokenizers for better results. I'm using a simple tokenizer.
     tensor input_token = text_vectorization(input_target, input, vocab_size, max_len);
     tensor target_token = text_vectorization(input_target, target, vocab_size, max_len);
 
