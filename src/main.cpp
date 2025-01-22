44d2b18ef0fd01f03af37534ffa1b0a80a30cb89
Sora <yukawa_hidenori@icloud.com>
2025-01-22 23:44:49 +0900

    add a todo

--- src/main.cpp
+++ src/main.cpp
@@ -69,30 +69,27 @@ tensor predict(const tensor& x_test, const tensor& y_test) {
 }
 
 int main() {
+    // TODO: If I make load_daily_dialog() return input and target by utlizing " in the datset, I only need to call this function once which improve performance a lot.
+    auto input_target = load_daily_dialog("datasets/daily_dialog/daily_dialog.csv");
+    auto input = load_daily_dialog("datasets/daily_dialog/daily_dialog_input.csv");
+    auto target = load_daily_dialog("datasets/daily_dialog/daily_dialog_target.csv");
 
-    auto a = variable({2, 3}, {1, 2, 3, 1, 2, 3});
-    auto v = embedding(10, 3, a);
+    // TODO: If I make text_vectorization() a class, runtime will be 1/2 of now as I only need to create the vocabulary once for "input_target". I don't need to do it twice.
+    // TODO: I may need to use subword tokenizers for better results. I'm using a simple tokenizer.
+    tensor input_token = text_vectorization(input_target, input, vocab_size, max_len);
+    tensor target_token = text_vectorization(input_target, target, vocab_size, max_len);
 
-    std::cout << v.mat << "\n";
-    std::cout << v.dense_vecs << "\n";
+    tensor dammy_input_token = zeros({10, 25});
+    tensor dammy_target_token = zeros({10, 25});
 
-    // auto input_target = load_daily_dialog("datasets/daily_dialog/daily_dialog.csv");
-    // auto input = load_daily_dialog("datasets/daily_dialog/daily_dialog_input.csv");
-    // auto target = load_daily_dialog("datasets/daily_dialog/daily_dialog_target.csv");
+    auto input_token_train_test = split(dammy_input_token, 0.2f);
+    auto target_token_train_test = split(dammy_target_token, 0.2f);
 
-    // // TODO: If I make text_vectorization() a class, runtime will be 1/2 of now as I only need to create the vocabulary once for "input_target". I don't need to do it twice.
-    // // TODO: I may need to use subword tokenizers for better results. I'm using a simple tokenizer.
-    // tensor input_token = text_vectorization(input_target, input, vocab_size, max_len);
-    // tensor target_token = text_vectorization(input_target, target, vocab_size, max_len);
+    train(input_token_train_test.first, target_token_train_test.first);
 
-    // auto input_token_train_test = split(input_token, 0.2f);
-    // auto target_token_train_test = split(target_token, 0.2f);
+    std::cout << "Test loss: " << evaluate(input_token_train_test.second, target_token_train_test.second) << "\n\n";
 
-    // train(input_token_train_test.first, target_token_train_test.first);
-
-    // std::cout << "Test loss: " << evaluate(input_token_train_test.second, target_token_train_test.second) << "\n\n";
-
-    // tensor test_predictions = predict(input_token_train_test.second, target_token_train_test.second);
+    tensor test_predictions = predict(input_token_train_test.second, target_token_train_test.second);
 
     return 0;
 }
