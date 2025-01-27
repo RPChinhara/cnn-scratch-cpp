2be3995386cf39032c20b9a1d394e42fbf5e7194
Sora <yukawa_hidenori@icloud.com>
2025-01-27 17:58:14 +0900

    house keeping

--- tests/embedding.cpp
+++ tests/embedding.cpp
@@ -5,9 +5,12 @@
 #include <iostream>
 
 int main() {
+    size_t embedding_dim = 3;
+    size_t vocab_size = 10;
+
     auto input = variable({2, 3}, {0, 2, 3,
-                                   7, 4, 8});
-    embedding lyr = embedding(10, 3, input);
+                                   7, 4, 9});
+    embedding lyr = embedding(vocab_size, embedding_dim, input);
 
     std::cout << lyr.mat << "\n";
     std::cout << lyr.dense_vecs << "\n";
