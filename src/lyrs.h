e890607200337056e124167a9948b1825d45fe82
Sora <yukawa_hidenori@icloud.com>
2025-01-27 21:21:57 +0900

    change name

--- src/lyrs.h
+++ src/lyrs.h
@@ -6,7 +6,7 @@
 class embedding {
   public:
     tensor embedding_mat;
-    tensor dense_vecs;
+    tensor embedded_tokens;
 
     embedding(const size_t vocab_size, const size_t embedding_dim, const tensor& t);
 };
