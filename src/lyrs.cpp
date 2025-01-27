48a8eb0c7033b5dcb51a4f73858cc0f5f53c81bc
Sora <yukawa_hidenori@icloud.com>
2025-01-27 21:25:11 +0900

    house keeping

--- src/lyrs.cpp
+++ src/lyrs.cpp
@@ -7,15 +7,16 @@
 #include <unordered_map>
 
 embedding::embedding(const size_t vocab_size, const size_t embedding_dim, const tensor& t) {
-    mat = uniform_dist({vocab_size, embedding_dim});
+    embedding_mat = uniform_dist({vocab_size, embedding_dim});
 
-    dense_vecs = zeros({t.shape.front(), t.shape.back(), embedding_dim});
+    embedded_tokens = zeros({t.shape.front(), t.shape.back(), embedding_dim});
 
     for (auto i = 0; i < t.size; ++i) {
-        auto a = slice(mat, t[i], 1);
+        auto embedding_vec = slice(embedding_mat, t[i], 1);
 
-        for (auto j = 0; j < a.size; ++j)
-            dense_vecs[embedding_dim * i + j] = a[j];
+        for (auto j = 0; j < embedding_vec.size; ++j) {
+            embedded_tokens[i * embedding_dim + j] = embedding_vec[j];
+        }
     }
 }
 
