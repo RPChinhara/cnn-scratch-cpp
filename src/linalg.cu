4040639e4f184ad7a83e0e0439ee19e1e1d25c04
Sora <yukawa_hidenori@icloud.com>
2025-02-03 12:04:27 +0900

    support 3d matmul!

--- src/linalg.cu
+++ src/linalg.cu
@@ -3,7 +3,7 @@
 #include "math.h"
 #include "tensor.h"
 
-__global__ void matmul(float* a, float* b, float* c, int m, int n, int p) {
+__global__ void matmul_kernel(float* a, float* b, float* c, int m, int n, int p) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -17,14 +17,14 @@ __global__ void matmul(float* a, float* b, float* c, int m, int n, int p) {
     }
 }
 
-tensor matmul(const tensor& t1, const tensor& t2) {
-    tensor t_new = zeros({t1.shape.front(), t2.shape.back()});
+tensor matmul_cuda(const tensor& t1, const tensor& t2) {
+    size_t m = t1.shape.front();
+    size_t n = t1.shape.back();
+    size_t p = t2.shape.back();
 
-    int m = t1.shape.front();
-    int n = t1.shape.back();
-    int p = t2.shape.back();
+    tensor t_new = zeros({m, p});
 
-    float* d_a, * d_b, * d_c;
+    float *d_a, *d_b, *d_c;
     cudaMalloc(&d_a, m * n * sizeof(float));
     cudaMalloc(&d_b, n * p * sizeof(float));
     cudaMalloc(&d_c, m * p * sizeof(float));
@@ -33,15 +33,43 @@ tensor matmul(const tensor& t1, const tensor& t2) {
     cudaMemcpy(d_b, t2.elems, n * p * sizeof(float), cudaMemcpyHostToDevice);
 
     dim3 threadsPerBlock(16, 16);
-    dim3 numBlocks((p + threadsPerBlock.x - 1) / threadsPerBlock.x,(m + threadsPerBlock.y - 1) / threadsPerBlock.y);
+    dim3 numBlocks((p + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                   (m + threadsPerBlock.y - 1) / threadsPerBlock.y);
 
-    matmul<<<numBlocks, threadsPerBlock>>>(d_a, d_b, d_c, m, n, p);
+    matmul_kernel<<<numBlocks, threadsPerBlock>>>(d_a, d_b, d_c, m, n, p);
 
     cudaMemcpy(t_new.elems, d_c, m * p * sizeof(float), cudaMemcpyDeviceToHost);
 
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
+
+    return t_new;
+}
+
+tensor matmul(const tensor& t1, const tensor& t2) {
+    if (t1.shape.size() == 2) {
+        return matmul_cuda(t1, t2);
+    }
+
+    size_t batch_size = 1;
+    for (size_t i = 0; i < t1.shape.size() - 2; ++i) {
+        batch_size *= t1.shape[i];
+    }
+
+    size_t t1_row = t1.shape[t1.shape.size() - 2];
+    size_t t2_row = t2.shape[t2.shape.size() - 2];
+    size_t t2_col = t2.shape.back();
+
+    tensor t_new = zeros({batch_size, t1_row, t2_col});
+
+    for (size_t i = 0; i < batch_size; ++i) {
+        tensor mat_t1 = slice(t1, i * t1_row, t1_row);
+        tensor mat_t2 = slice(t2, i * t2_row, t2_row);
+        tensor output = matmul_cuda(mat_t1, mat_t2);
+
+        std::copy(output.elems, output.elems + output.size, t_new.elems + i * output.size);
+    }
 
     return t_new;
 }
