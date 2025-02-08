8fdde4430848b20372cf630155dfd7699974df1f
Sora <yukawa_hidenori@icloud.com>
2025-02-08 19:24:45 +0900

    use new text_vectorizer

--- examples/nlp/text_vectorization.cpp
+++ examples/nlp/text_vectorization.cpp
@@ -4,7 +4,13 @@ int main() {
     std::vector<std::string> vocab = {"foo bar", "bar baz", "baz bada boom"};
     std::vector<std::string> in = {"foo qux bar", "qux baz"};
 
-    std::cout << text_vectorization(vocab, in, 5000, 4) << std::endl;
+    size_t vocab_size = 5000;
+    size_t seq_len = 4;
+
+    text_vectorizer vectorizer(vocab_size, seq_len);
+    vectorizer.build_vocab(vocab);
+
+    std::cout << vectorizer.vectorize(in) << "\n";
 
     return 0;
 }
