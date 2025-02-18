63640301847f796b1e9eb37e681e6e3f4f8c4328
Sora <yukawa_hidenori@icloud.com>
2025-02-18 20:46:40 +0900

    test for the loss

---
+++ tests/sparse_categorical_cross_entropy.cpp
@@ -0,0 +1,10 @@
+#include "losses.h"
+
+int main() {
+    tensor y_true = variable({1, 2}, {1, 2});
+    tensor y_pred = variable({2, 3}, {0.05, 0.95, 0, 0.1, 0.8, 0.1});
+
+    std::cout <<  << "\n";
+
+    return 0;
+}
