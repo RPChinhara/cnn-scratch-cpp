5e94a2279e3fb4f6b2d0073615aa134a0c6e90f0
Sora <yukawa_hidenori@icloud.com>
2025-02-18 20:51:50 +0900

    house keeping

--- tests/sparse_categorical_cross_entropy.cpp
+++ tests/sparse_categorical_cross_entropy.cpp
@@ -1,10 +1,12 @@
+#include "arrs.h"
 #include "losses.h"
+#include "tensor.h"
 
 int main() {
-    tensor y_true = variable({1, 2}, {1, 2});
-    tensor y_pred = variable({2, 3}, {0.05, 0.95, 0, 0.1, 0.8, 0.1});
+    tensor y_true = variable({1, 2}, {1.0f, 2.0f});
+    tensor y_pred = variable({2, 3}, {0.05f, 0.95f, 0.0f, 0.1f, 0.8f, 0.1f});
 
-    std::cout <<  << "\n";
+    std::cout << sparse_categorical_cross_entropy(y_true, y_pred) << "\n";
 
     return 0;
 }
