3d8e77bdf60589ae09670a530ec3b4fad83c1e4f
Sora <yukawa_hidenori@icloud.com>
2025-02-18 21:03:31 +0900

    batch dim comes first

--- tests/sparse_categorical_cross_entropy.cpp
+++ tests/sparse_categorical_cross_entropy.cpp
@@ -3,7 +3,7 @@
 #include "tensor.h"
 
 int main() {
-    tensor y_true = variable({1, 2}, {1.0f, 2.0f});
+    tensor y_true = variable({2, 1}, {1.0f, 2.0f});
     tensor y_pred = variable({2, 3}, {0.05f, 0.95f, 0.0f, 0.1f, 0.8f, 0.1f});
 
     std::cout << sparse_categorical_cross_entropy(y_true, y_pred) << "\n";
