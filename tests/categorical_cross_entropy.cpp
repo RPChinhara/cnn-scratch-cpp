cb5321541dd58790f45a0447faf5636b473f87b3
Sora <yukawa_hidenori@icloud.com>
2025-02-18 21:48:24 +0900

    house keeping

--- tests/categorical_cross_entropy.cpp
+++ tests/categorical_cross_entropy.cpp
@@ -3,10 +3,10 @@
 #include "tensor.h"
 
 int main() {
-    tensor y_true2 = variable({2, 3}, {0, 1, 0, 0, 0, 1});
-    tensor y_pred2 = variable({2, 3}, {0.05f, 0.95f, 0.0f, 0.1f, 0.8f, 0.1f});
+    tensor y_true = variable({2, 3}, {0, 1, 0, 0, 0, 1});
+    tensor y_pred = variable({2, 3}, {0.05f, 0.95f, 0.0f, 0.1f, 0.8f, 0.1f});
 
-    std::cout << categorical_cross_entropy(y_true2, y_pred2) << "\n";
-    
+    std::cout << categorical_cross_entropy(y_true, y_pred) << "\n";
+
     return 0;
 }
