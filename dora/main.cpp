e8ac21409c803407095a6804617603617a841825
Sora <yukawa_hidenori@icloud.com>
2025-02-21 17:08:21 +0900

    add the model

---
+++ dora/main.cpp
@@ -0,0 +1,16 @@
+#include "rand.h"
+#include "tensor.h"
+
+tensor w1 = glorot_uniform({32, 32});
+tensor w2 = glorot_uniform({32, 32});
+tensor w3 = glorot_uniform({32, 32});
+tensor w4 = glorot_uniform({32, 32});
+
+int main() {
+
+    while (true) {
+        std::cout << "Learning...\n";
+    }
+
+    return 0;
+}
