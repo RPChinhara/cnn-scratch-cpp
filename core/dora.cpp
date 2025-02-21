0bcde56c8961c3568384e77252e9d46c3c291b6f
Sora <yukawa_hidenori@icloud.com>
2025-02-21 17:16:00 +0900

    update it

--- core/dora.cpp
+++ core/dora.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
 #include "rand.h"
 #include "tensor.h"
 
@@ -7,10 +11,9 @@ tensor w3 = glorot_uniform({32, 32});
 tensor w4 = glorot_uniform({32, 32});
 
 int main() {
-
     while (true) {
         std::cout << "Learning...\n";
+        std::this_thread::sleep_for(std::chrono::seconds(5));  // Sleep for 10 seconds
     }
-
     return 0;
 }
