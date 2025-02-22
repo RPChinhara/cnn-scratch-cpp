4b3c3bc5bce70f0159151fedeaade9f06c441646
Sora <yukawa_hidenori@icloud.com>
2025-02-22 23:51:33 +0900

    make a window!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <thread>
+#include <windows.h>
 
 #include "rand.h"
 #include "tensor.h"
@@ -11,13 +12,44 @@ tensor synapse4 = glorot_uniform({32, 32});
 
 // TODO: I have to make env in 3D...
 
-int main() {
-    while (true) {
-        std::cout << "Processing inputs...\n";
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+// Window Procedure: Handles window events
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            return 0;
+        case WM_DESTROY:
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
-        std::cout << "Backpropagating...\n\n";
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Define window class
+    const char CLASS_NAME[] = "MyWindowClass";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0, CLASS_NAME, "My Window", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, // x, y, width, height
+        NULL, NULL, hInstance, NULL);
+
+    if (!hwnd) return 0;
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Message loop
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
     return 0;
