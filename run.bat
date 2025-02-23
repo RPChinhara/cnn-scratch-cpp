e0e69ff5903f5b8522097be927ea1f3f593d74cf
Sora <yukawa_hidenori@icloud.com>
2025-02-23 16:23:25 +0900

    add window file

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 set NVCC=nvcc
 set FLAGS=-arch sm_75 -o dora -O3 -std c++20 --threads 0 --use_fast_math -Xcompiler "/MP /MT /O2"
-set SRC=..\src\acts.cpp ..\src\arrs.cpp ..\src\datasets.cpp ..\src\linalg.cu ..\src\losses.cpp ..\src\lyrs.cpp ..\src\main.cpp ..\src\math.cu ..\src\rand.cpp ..\src\strings.cpp ..\src\tensor.cpp user32.lib gdi32.lib d3d11.lib D3DCompiler.lib
+set SRC=..\src\acts.cpp ..\src\arrs.cpp ..\src\datasets.cpp ..\src\linalg.cu ..\src\losses.cpp ..\src\lyrs.cpp ..\src\main.cpp ..\src\math.cu ..\src\rand.cpp ..\src\strings.cpp ..\src\tensor.cpp ..\src\window.cpp user32.lib gdi32.lib d3d11.lib D3DCompiler.lib
 
 if not exist bin mkdir bin
 
