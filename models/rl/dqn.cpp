7be70120725627a91e6468dee68dfdb4c3a0c65b
Sora <yukawa_hidenori@icloud.com>
2025-02-23 18:36:39 +0900

    house keeping

---
+++ models/rl/dqn.cpp
@@ -0,0 +1,18 @@
+#include "tensor.h"
+
+// NOTE: Use this style instead of weights or w this is more intuitive?
+size_t synapse1 = 32;
+size_t synapse2 = 32;
+size_t synapse3 = 32;
+size_t synapse4 = 32;
+size_t synapse5 = 32;
+
+tensor neuron1 = glorot_uniform({synapse1, synapse2});
+tensor neuron2 = glorot_uniform({synapse2, synapse3});
+tensor neuron3 = glorot_uniform({synapse3, synapse4});
+tensor neuron4 = glorot_uniform({synapse4, synapse5});
+
+int main() {
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,6 @@
 #include "tensor.h"
 #include "window.h"
 
-size_t synapse1 = 32;
-size_t synapse2 = 32;
-size_t synapse3 = 32;
-size_t synapse4 = 32;
-size_t synapse5 = 32;
-
-tensor neuron1 = glorot_uniform({synapse1, synapse2});
-tensor neuron2 = glorot_uniform({synapse2, synapse3});
-tensor neuron3 = glorot_uniform({synapse3, synapse4});
-tensor neuron4 = glorot_uniform({synapse4, synapse5});
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     AllocConsole();
     freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
