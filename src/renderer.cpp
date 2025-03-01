b1c8abde1501e02839342c045521d5624ab4e15d
mon <yukawa_hidenori@icloud.com>
2025-03-01 17:25:02 +0900

    disable culling

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -104,7 +104,8 @@ bool renderer::create_depth_stencil_state() {
 bool renderer::create_rasterizer_state() {
     D3D11_RASTERIZER_DESC rasterizer_desc = {};
     rasterizer_desc.FillMode = D3D11_FILL_SOLID;
-    rasterizer_desc.CullMode = D3D11_CULL_BACK;
+    // rasterizer_desc.CullMode = D3D11_CULL_BACK;
+    rasterizer_desc.CullMode = D3D11_CULL_NONE;  // Disable culling to see all faces
     rasterizer_desc.FrontCounterClockwise = FALSE;  // For clockwise winding
 
     HRESULT hr = device->CreateRasterizerState(&rasterizer_desc, rasterizer_state.GetAddressOf());
