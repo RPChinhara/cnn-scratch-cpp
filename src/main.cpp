fec1c9096236cffda5b694a1d81b10741e6803cf
Sora <yukawa_hidenori@icloud.com>
2025-03-05 03:56:46 +0900

    rm logger and house keeping

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
-#include "arrs.h"
 #include "camera.h"
 #include "input_handler.h"
-#include "logger.h"
 #include "mesh.h"
 #include "renderer.h"
 #include "scene.h"
@@ -9,19 +7,20 @@
 #include "window.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    logger::init();
-    logger::log(fill({2, 3}, 1.0f));
+    AllocConsole();
+    freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
 
     window window(hInstance);
-
     renderer r(window.get_hwnd());
-    if (!r.init()) return -1;
-
-    input_handler input;
     camera cam;
-
+    input_handler input;
     scene main_scene;
-    if (!main_scene.load(&r)) return -1;
+
+    if (!r.init())
+        return -1;
+
+    if (!main_scene.load(&r))
+        return -1;
 
     while (window.process_messages()) {
         input.update(cam);
