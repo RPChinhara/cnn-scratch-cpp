983c4f0b5da7b23ad2bc5eec8f82d5cbd4a65e26
Sora <yukawa_hidenori@icloud.com>
2025-03-05 17:56:27 +0900

    rm regex_replace()

--- src/strings.cpp
+++ src/strings.cpp
@@ -3,11 +3,6 @@
 #include <regex>
 #include <sstream>
 
-std::string regex_replace(const std::string& in, const std::string& pattern, const std::string& rewrite) {
-    std::regex re(pattern);
-    return std::regex_replace(in, re, rewrite);
-}
-
 std::vector<std::string> tokenizer(const std::string& text) {
     std::vector<std::string> tokens;
     std::stringstream ss(text);
