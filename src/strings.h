6d99c83b96db7ddf5dddcaa95c19b546f7d7e7d5
Sora <yukawa_hidenori@icloud.com>
2025-03-05 17:52:48 +0900

    move lower()

--- src/strings.h
+++ src/strings.h
@@ -5,6 +5,5 @@
 #include <string>
 #include <vector>
 
-std::string lower(const std::string& text);
 std::string regex_replace(const std::string& in, const std::string& pattern, const std::string& rewrite);
 std::vector<std::string> tokenizer(const std::string& text);
