7ab63a02a6c73d0a8d6c4404280cc72a38a8fe1b
Sora <yukawa_hidenori@icloud.com>
2025-03-05 18:01:25 +0900

    move tokenizer

--- src/strings.cpp
+++ src/strings.cpp
@@ -1,16 +1,4 @@
 #include "string.h"
 
 #include <regex>
-#include <sstream>
 
-std::vector<std::string> tokenizer(const std::string& text) {
-    std::vector<std::string> tokens;
-    std::stringstream ss(text);
-    std::string token;
-
-    while (ss >> token) {
-        tokens.push_back(token);
-    }
-
-    return tokens;
-}
